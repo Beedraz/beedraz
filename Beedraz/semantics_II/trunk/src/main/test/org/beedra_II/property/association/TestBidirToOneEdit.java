e21f21f4cd87bd3dc0bea3382358df6af9ed5003
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-02-21 10:39:24 +0000

    Added more cases to notifyListeners.

--- Beedraz/semantics_II/trunk/src/main/test/org/beedra_II/property/association/TestBidirToOneEdit.java
+++ Beedraz/semantics_II/trunk/src/main/test/org/beedra_II/property/association/TestBidirToOneEdit.java
@@ -30,6 +30,7 @@ import org.beedra_II.edit.IllegalEditException;
 import org.beedra_II.edit.ValidityListener;
 import org.beedra_II.edit.Edit.State;
 import org.beedra_II.event.Listener;
+import org.beedra_II.property.set.SetEvent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -128,6 +129,20 @@ public class TestBidirToOneEdit {
 
   }
 
+  public class StubBidirToManyListener implements Listener<SetEvent<ManyBeanBeed, BidirToOneEdit<OneBeanBeed, ManyBeanBeed>>> {
+
+    public void beedChanged(SetEvent<ManyBeanBeed, BidirToOneEdit<OneBeanBeed, ManyBeanBeed>> event) {
+      $event = event;
+    }
+
+    public void reset() {
+      $event = null;
+    }
+
+    public SetEvent<ManyBeanBeed, BidirToOneEdit<OneBeanBeed, ManyBeanBeed>> $event;
+
+  }
+
   @Before
   public void setUp() throws Exception {
     // NOP
@@ -146,6 +161,8 @@ public class TestBidirToOneEdit {
   StubValidityListener $listener1 = new StubValidityListener();
   StubValidityListener $listener2 = new StubValidityListener();
   StubBidirToOneListener $listener3 = new StubBidirToOneListener();
+  StubBidirToManyListener $listener4 = new StubBidirToManyListener();
+  StubBidirToManyListener $listener5 = new StubBidirToManyListener();
 
   @Test
   public void constructor() {
@@ -849,22 +866,52 @@ public class TestBidirToOneEdit {
   }
 
   @Test
-  public void notifyListeners() throws EditStateException {
+  public void notifyListeners() throws EditStateException, IllegalEditException {
+    // add initial goal
+    BidirToManyBeed<OneBeanBeed, ManyBeanBeed> goal1 =
+      new BidirToManyBeed<OneBeanBeed, ManyBeanBeed>($oneBeanBeed);
+    MyBidirToOneEdit bidirToOneEdit = new MyBidirToOneEdit($target);
+    bidirToOneEdit.setGoal(goal1);
+    bidirToOneEdit.perform();
+    assertEquals($target.get(), goal1);
+    // create second goal
+    BidirToManyBeed<OneBeanBeed, ManyBeanBeed> goal2 =
+      new BidirToManyBeed<OneBeanBeed, ManyBeanBeed>($oneBeanBeed);
     // add listener to beed
     $target.addListener($listener3);
     assertTrue($target.isListener($listener3));
     assertNull($listener3.$event);
+    // add listener to goal1 and goal2
+    goal1.addListener($listener4);
+    goal2.addListener($listener5);
+    assertTrue(goal1.isListener($listener4));
+    assertTrue(goal2.isListener($listener5));
+    assertNull($listener4.$event);
+    assertNull($listener5.$event);
     // notify
-    BidirToManyBeed<OneBeanBeed, ManyBeanBeed> goal =
-      new BidirToManyBeed<OneBeanBeed, ManyBeanBeed>($oneBeanBeed);
-    $bidirToOneEdit.setGoal(goal);
+    $bidirToOneEdit.setGoal(goal2);
+    $bidirToOneEdit.storeInitialStatePublic();
     $bidirToOneEdit.notifyListenersPublic();
-    // check
+    // check whether the listener of the beed is notified
     assertNotNull($listener3.$event);
     assertEquals($listener3.$event.getEdit(), $bidirToOneEdit);
-    assertEquals($listener3.$event.getOldValue(), goal);
-    assertEquals($listener3.$event.getNewValue(), null);
+    assertEquals($listener3.$event.getOldValue(), goal2);
+    assertEquals($listener3.$event.getNewValue(), goal1);
     assertEquals($listener3.$event.getSource(), $target);
+    // check whether the listener of goal1 is notified
+    assertNotNull($listener4.$event);
+    assertEquals($listener4.$event.getEdit(), $bidirToOneEdit);
+    assertTrue($listener4.$event.getAddedElements().size() == 1);
+    assertTrue($listener4.$event.getAddedElements().contains($target.getOwner()));
+    assertTrue($listener4.$event.getRemovedElements().isEmpty());
+    assertEquals($listener4.$event.getSource(), goal1);
+    // check whether the listener of goal2 is notified
+    assertNotNull($listener5.$event);
+    assertEquals($listener5.$event.getEdit(), $bidirToOneEdit);
+    assertTrue($listener5.$event.getAddedElements().isEmpty());
+    assertTrue($listener5.$event.getRemovedElements().size() == 1);
+    assertTrue($listener5.$event.getRemovedElements().contains($target.getOwner()));
+    assertEquals($listener5.$event.getSource(), goal2);
   }
 
   @Test

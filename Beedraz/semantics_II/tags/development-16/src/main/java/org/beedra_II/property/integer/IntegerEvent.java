20ce971fab6ac8b3095d7ac7549f2bdf54547204
cvs2svn <cvs2svn@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-02 15:05:17 +0000

    This commit was manufactured by cvs2svn to create tag 'development-16'.

---
+++ Beedraz/semantics_II/tags/development-16/src/main/java/org/beedra_II/property/integer/IntegerEvent.java
@@ -0,0 +1,85 @@
+/*<license>
+Copyright 2007 - $Date$ by the authors mentioned below.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.beedra_II.property.integer;
+
+
+import static org.beedra.util_I.MultiLineToStringUtil.indent;
+
+import org.beedra_II.edit.Edit;
+import org.beedra_II.property.simple.OldNewEvent;
+import org.toryt.util_I.annotations.vcs.CvsInfo;
+
+
+/**
+ * {@link OldNewEvent} whose source is a {@link IntegerBeed} and
+ * that carries a simple old and new value of type {@link Integer}.
+ *
+ * @author Jan Dockx
+ *
+ * @invar getSource() instanceof IntegerBeed;
+ */
+@CvsInfo(revision = "$Revision$",
+         date     = "$Date$",
+         state    = "$State$",
+         tag      = "$Name$")
+public final class IntegerEvent extends OldNewEvent<Integer> {
+
+  /**
+   * @pre  source != null;
+   * @pre  edit != null;
+   * @pre  (edit.getState() == DONE) || (edit.getState() == UNDONE);
+   * @pre  (oldValue != null) && (newValue != null)
+   *          ? ! oldValue.equals(newValue)
+   *          : true;
+   *
+   * @post getSource() == source;
+   * @post getEdit() == edit;
+   * @post getEditState() == edit.getState();
+   * @post oldValue == null ? getOldValue() == null : getOldValue().equals(oldValue);
+   * @post newValue == null ? getNewValue() == null : getNewValue().equals(newValue);
+   * @post oldValue == null || newValue == null
+   *          ? getDelta() == null
+   *          : getDelta() = newValue - oldValue;
+   */
+  public IntegerEvent(IntegerBeed source, Integer oldValue, Integer newValue, Edit<?> edit) {
+    super(source, oldValue, newValue, edit);
+    $delta = ((oldValue == null) || (newValue == null)) ? null : newValue - oldValue; // MUDO overflow
+  }
+
+  /**
+   * @return ((getOldValue() == null) || (getNewValue() == null)) ? null : getNewValue() - getOldValue();
+   */
+  public final Integer getDelta() {
+    return $delta;
+  }
+
+  private final Integer $delta;
+
+  @Override
+  protected String otherToStringInformation() {
+    return super.otherToStringInformation() +
+           ", delta: " + getDelta();
+  }
+
+  @Override
+  public void toString(StringBuffer sb, int level) {
+    super.toString(sb, level);
+    sb.append(indent(level + 1) + "delta:" + getDelta() + "\n");
+  }
+
+}
+
---
+++ Beedraz/semantics_II/tags/development-16/src/main/java/org/beedra_II/topologicalupdate/AbstractUpdateSource.java
@@ -0,0 +1,75 @@
+/*<license>
+Copyright 2007 - $Date$ by the authors mentioned below.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.beedra_II.topologicalupdate;
+
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.toryt.util_I.annotations.vcs.CvsInfo;
+
+
+/**
+ * @author Jan Dockx
+ */
+@CvsInfo(revision = "$Revision$",
+         date     = "$Date$",
+         state    = "$State$",
+         tag      = "$Name$")
+public abstract class AbstractUpdateSource implements UpdateSource {
+
+  public final Set<Dependent<?>> getDependents() {
+    return Collections.unmodifiableSet($dependents);
+  }
+
+  public final Set<Dependent<?>> getDependentsTransitiveClosure() {
+    Set<Dependent<?>> dtc = new HashSet<Dependent<?>>();
+    dtc.addAll($dependents);
+    for (Dependent<?> dependent : $dependents) {
+      dependentsTransitiveClosure(dependent, dtc);
+    }
+    return dtc;
+  }
+
+  private final void dependentsTransitiveClosure(Dependent<?> dependent, Set<Dependent<?>> acc) {
+    assert acc.contains(dependent);
+    for (Dependent<?> secondDependent : dependent.getDependents()) {
+      if (! acc.contains(secondDependent)) {
+        acc.add(secondDependent);
+        dependentsTransitiveClosure(secondDependent, acc);
+      }
+    }
+  }
+
+  public final void addDependent(Dependent<?> dependent) {
+    assert dependent != null;
+    assert ! dependent.getDependentsTransitiveClosure().contains(this);
+    $dependents.add(dependent);
+  }
+
+  public final void removeDependent(Dependent<?> dependent) {
+    $dependents.remove(dependent);
+  }
+
+  /**
+   * @invar $dependents != null;
+   * @invar Collections.noNull($dependents);
+   */
+  private final Set<Dependent<?>> $dependents = new HashSet<Dependent<?>>();
+
+}

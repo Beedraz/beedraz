a05551833a2a449c3b47782a98228aa753c80c56
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-14 12:50:34 +0000

    Moved duplicate code to superclass.

--- Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/decimal/DoubleMeanBeed.java
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/decimal/DoubleMeanBeed.java
@@ -17,28 +17,14 @@ limitations under the License.
 package org.beedra_II.property.decimal;
 
 
-import static org.ppeew.smallfries_I.MultiLineToStringUtil.indent;
-
-import java.util.Set;
-
 import org.beedra_II.aggregate.AggregateBeed;
-import org.beedra_II.event.Listener;
-import org.beedra_II.property.AbstractPropertyBeed;
-import org.beedra_II.property.collection.set.SetBeed;
-import org.beedra_II.property.collection.set.SetEvent;
 import org.ppeew.annotations_I.vcs.CvsInfo;
-import org.ppeew.smallfries_I.ComparisonUtil;
 
 
 /**
- * A beed that computes the mean of a given set of beeds of type {@link DoubleBeed}.
+ * A beed that computes the mean of a given set of beeds of type
+ * {@link DoubleBeed}.
  *
- * @invar getSource() == null ==> getDouble() == null;
- * @invar getSource() != null ==>
- *        (exists DoubleBeed db; getSource().get().contains(db); db.getDouble() == null)
- *            ==> getDouble() == null;
- *        If the value of one of the beeds in the given set is null,
- *        then the value of the mean beed is null.
  * @invar getSource() != null ==>
  *        (forAll DoubleBeed db; getSource().get().contains(db); db.getDouble() != null)
  *            ==> getDouble() == avg { db.getDouble() | getSource().get().contains(db)};
@@ -51,9 +37,7 @@ import org.ppeew.smallfries_I.ComparisonUtil;
          date     = "$Date$",
          state    = "$State$",
          tag      = "$Name$")
-public class DoubleMeanBeed
-    extends AbstractPropertyBeed<DoubleEvent>
-    implements DoubleBeed<DoubleEvent> {
+public class DoubleMeanBeed extends DoubleSetComputationBeed {
 
 
   /**
@@ -65,112 +49,6 @@ public class DoubleMeanBeed
     super(owner);
   }
 
-
-  /*<property name="source">*/
-  //------------------------------------------------------------------
-
-  /**
-   * @basic
-   */
-  public final SetBeed<DoubleBeed<DoubleEvent>, ?> getSource() {
-    return $source;
-  }
-
-  /**
-   * @param   source
-   * @post    getSource() == source;
-   * @post    getDouble() == the mean of the given source
-   * @post    The DoubleMeanBeed is registered as a listener of the given SetBeed.
-   * @post    The DoubleMeanBeed is registered as a listener of all DoubleBeeds in
-   *          the given SetBeed. (The reason is that the DoubleMeanBeed should be
-   *          notified (and then recalculate) when one of the DoubleBeeds changes.)
-   * @post    The listeners of this beed are notified when the value changes.
-   */
-  public final void setSource(SetBeed<DoubleBeed<DoubleEvent>, ?> source) {
-    // set the source
-    $source = source;
-    if (source != null) {
-      // register the DoubleMeanBeed as listener of the given SetBeed
-      source.addListener($sourceListener);
-      // register the DoubleMeanBeed as listener of all DoubleBeeds in the given SetBeed
-      for (DoubleBeed<DoubleEvent> beed : source.get()) {
-        beed.addListener($beedListener);
-      }
-    }
-    // recalculate and notify the listeners if the value has changed
-    Double oldValue = $value;
-    recalculate();
-    if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-      fireChangeEvent(new ActualDoubleEvent(DoubleMeanBeed.this, oldValue, $value, null)); // edit = null
-    }
-  }
-
-  private SetBeed<DoubleBeed<DoubleEvent>, ?> $source;
-
-  /*</property>*/
-
-
-  /**
-   * A listener that will be registered as listener of the {@link #getSource()}.
-   */
-  private final Listener<SetEvent<DoubleBeed<DoubleEvent>>> $sourceListener =
-        new Listener<SetEvent<DoubleBeed<DoubleEvent>>>() {
-
-    /**
-     * @post    The DoubleMeanBeed is registered as a listener of all DoubleBeeds
-     *          that are added to the SetBeed by the given event. (The reason is that
-     *          the DoubleMeanBeed should be notified (and then recalculate) when one
-     *          of the DoubleBeeds changes.)
-     * @post    The DoubleMeanBeed is removed as listener of all DoubleBeeds
-     *          that are removed from the SetBeed by the given event.
-     * @post    getDouble() == the mean of the given source
-     * @post    The listeners of this beed are notified when the value changes.
-     */
-    public void beedChanged(SetEvent<DoubleBeed<DoubleEvent>> event) {
-      // add the DoubleMeanBeed as listener of all DoubleBeeds that are added to the SetBeed by the given event
-      Set<DoubleBeed<DoubleEvent>> added = event.getAddedElements();
-      for (DoubleBeed<DoubleEvent> beed : added) {
-        beed.addListener($beedListener);
-      }
-      // remove the DoubleMeanBeed as listener from all DoubleBeeds that are removed from the SetBeed by the given event
-      Set<DoubleBeed<DoubleEvent>> removed = event.getRemovedElements();
-      for (DoubleBeed<DoubleEvent> beed : removed) {
-        beed.removeListener($beedListener);
-      }
-      // recalculate and notify the listeners if the value has changed
-      Double oldValue = $value;
-      recalculate();
-      if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-        fireChangeEvent(new ActualDoubleEvent(DoubleMeanBeed.this, oldValue, $value, event.getEdit()));
-      }
-    }
-
-  };
-
-
-  private final Listener<DoubleEvent> $beedListener = new Listener<DoubleEvent>() {
-
-    /**
-     * @post    getDouble() == the mean of the given source
-     * @post    The listeners of this beed are notified when the value changes.
-     */
-    public void beedChanged(DoubleEvent event) {
-      // recalculate and notify the listeners if the value has changed
-      Double oldValue = $value;
-      recalculate();
-      if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-        fireChangeEvent(new ActualDoubleEvent(DoubleMeanBeed.this, oldValue, $value, event.getEdit()));
-      }
-    }
-
-  };
-
-
-  public final Double getDouble() {
-    return $value;
-  }
-
-
   /**
    * The value of $value is recalculated. This is done by iterating over the beeds
    * in the source set beed.
@@ -179,6 +57,7 @@ public class DoubleMeanBeed
    * When one of the terms is null, the result is null.
    * When all terms are effective, the result is the average of the values of the beeds.
    */
+  @Override
   public void recalculate() {
     Double newValue;
     if (getSource() == null) {
@@ -203,36 +82,7 @@ public class DoubleMeanBeed
         newValue = newValue / getSource().get().size(); // divisor is not zero (see if-condition)!
       }
     }
-    $value = newValue;
-  }
-
-
-  private Double $value = null;
-
-
-  /**
-   * @post  result != null;
-   * @post  result.getSource() == this;
-   * @post  result.getOldDouble() == null;
-   * @post  result.getNewDouble() == getDouble();
-   * @post  result.getEdit() == null;
-   * @post  result.getEditState() == null;
-   */
-  @Override
-  protected final DoubleEvent createInitialEvent() {
-    return new ActualDoubleEvent(this, null, getDouble(), null);
-  }
-
-  @Override
-  protected String otherToStringInformation() {
-    return getDouble() + " (# " + getSource().get().size() + ")";
-  }
-
-  @Override
-  public void toString(StringBuffer sb, int level) {
-    super.toString(sb, level);
-    sb.append(indent(level + 1) + "value:" + getDouble() + "\n");
-    sb.append(indent(level + 1) + "number of elements in source:" + getSource().get().size() + "\n");
+    setValue(newValue);
   }
 
 }
--- Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/decimal/DoubleStandardErrorBeed.java
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/decimal/DoubleStandardErrorBeed.java
@@ -17,28 +17,14 @@ limitations under the License.
 package org.beedra_II.property.decimal;
 
 
-import static org.ppeew.smallfries_I.MultiLineToStringUtil.indent;
-
-import java.util.Set;
-
 import org.beedra_II.aggregate.AggregateBeed;
-import org.beedra_II.event.Listener;
-import org.beedra_II.property.AbstractPropertyBeed;
-import org.beedra_II.property.collection.set.SetBeed;
-import org.beedra_II.property.collection.set.SetEvent;
 import org.ppeew.annotations_I.vcs.CvsInfo;
-import org.ppeew.smallfries_I.ComparisonUtil;
 
 
 /**
- * A beed that computes the standard error of a given set of beeds of type {@link DoubleBeed}.
+ * A beed that computes the standard error of a given set of beeds of type
+ * {@link DoubleBeed}.
  *
- * @invar getSource() == null ==> getDouble() == null;
- * @invar getSource() != null ==>
- *        (exists DoubleBeed db; getSource().get().contains(db); db.getDouble() == null)
- *            ==> getDouble() == null;
- *        If the value of one of the beeds in the given set is null,
- *        then the value of the standard error beed is null.
  * @invar getSource() != null ==>
  *        (forAll DoubleBeed db; getSource().get().contains(db); db.getDouble() != null)
  *            ==> getDouble() == SE { db.getDouble() | getSource().get().contains(db)};
@@ -56,9 +42,7 @@ import org.ppeew.smallfries_I.ComparisonUtil;
          date     = "$Date$",
          state    = "$State$",
          tag      = "$Name$")
-public class DoubleStandardErrorBeed
-    extends AbstractPropertyBeed<DoubleEvent>
-    implements DoubleBeed<DoubleEvent> {
+public class DoubleStandardErrorBeed extends DoubleSetComputationBeed {
 
 
   /**
@@ -68,111 +52,6 @@ public class DoubleStandardErrorBeed
    */
   public DoubleStandardErrorBeed(AggregateBeed owner) {
     super(owner);
-  }
-
-
-  /*<property name="source">*/
-  //------------------------------------------------------------------
-
-  /**
-   * @basic
-   */
-  public final SetBeed<DoubleBeed<DoubleEvent>, ?> getSource() {
-    return $source;
-  }
-
-  /**
-   * @param   source
-   * @post    getSource() == source;
-   * @post    getDouble() == the standard error of the given source
-   * @post    The DoubleStandardErrorBeed is registered as a listener of the given SetBeed.
-   * @post    The DoubleStandardErrorBeed is registered as a listener of all DoubleBeeds in
-   *          the given SetBeed. (The reason is that the DoubleStandardErrorBeed should be
-   *          notified (and then recalculate) when one of the DoubleBeeds changes.)
-   * @post    The listeners of this beed are notified when the value changes.
-   */
-  public final void setSource(SetBeed<DoubleBeed<DoubleEvent>, ?> source) {
-    // set the source
-    $source = source;
-    if (source != null) {
-      // register the DoubleStandardErrorBeed as listener of the given SetBeed
-      source.addListener($sourceListener);
-      // register the DoubleStandardErrorBeed as listener of all DoubleBeeds in the given SetBeed
-      for (DoubleBeed<DoubleEvent> beed : source.get()) {
-        beed.addListener($beedListener);
-      }
-    }
-    // recalculate and notify the listeners if the value has changed
-    Double oldValue = $value;
-    recalculate();
-    if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-      fireChangeEvent(new ActualDoubleEvent(DoubleStandardErrorBeed.this, oldValue, $value, null)); // edit = null
-    }
-  }
-
-  private SetBeed<DoubleBeed<DoubleEvent>, ?> $source;
-
-  /*</property>*/
-
-
-  /**
-   * A listener that will be registered as listener of the {@link #getSource()}.
-   */
-  private final Listener<SetEvent<DoubleBeed<DoubleEvent>>> $sourceListener =
-        new Listener<SetEvent<DoubleBeed<DoubleEvent>>>() {
-
-    /**
-     * @post    The DoubleStandardErrorBeed is registered as a listener of all DoubleBeeds
-     *          that are added to the SetBeed by the given event. (The reason is that
-     *          the DoubleStandardErrorBeed should be notified (and then recalculate) when one
-     *          of the DoubleBeeds changes.)
-     * @post    The DoubleStandardErrorBeed is removed as listener of all DoubleBeeds
-     *          that are removed from the SetBeed by the given event.
-     * @post    getDouble() == the standard error of the given source
-     * @post    The listeners of this beed are notified when the value changes.
-     */
-    public void beedChanged(SetEvent<DoubleBeed<DoubleEvent>> event) {
-      // add the DoubleStandardErrorBeed as listener of all DoubleBeeds that are added to the SetBeed by the given event
-      Set<DoubleBeed<DoubleEvent>> added = event.getAddedElements();
-      for (DoubleBeed<DoubleEvent> beed : added) {
-        beed.addListener($beedListener);
-      }
-      // remove the DoubleStandardErrorBeed as listener from all DoubleBeeds that are removed from the SetBeed by the given event
-      Set<DoubleBeed<DoubleEvent>> removed = event.getRemovedElements();
-      for (DoubleBeed<DoubleEvent> beed : removed) {
-        beed.removeListener($beedListener);
-      }
-      // recalculate and notify the listeners if the value has changed
-      Double oldValue = $value;
-      recalculate();
-      if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-        fireChangeEvent(new ActualDoubleEvent(DoubleStandardErrorBeed.this, oldValue, $value, event.getEdit()));
-      }
-    }
-
-  };
-
-
-  private final Listener<DoubleEvent> $beedListener = new Listener<DoubleEvent>() {
-
-    /**
-     * @post    getDouble() == the standard error of the given source
-     * @post    The listeners of this beed are notified when the value changes.
-     */
-    public void beedChanged(DoubleEvent event) {
-      // recalculate and notify the listeners if the value has changed
-      Double oldValue = $value;
-      recalculate();
-      if (! ComparisonUtil.equalsWithNull(oldValue, $value)) {
-        fireChangeEvent(new ActualDoubleEvent(DoubleStandardErrorBeed.this, oldValue, $value, event.getEdit()));
-      }
-    }
-
-  };
-
-
-  public final Double getDouble() {
-    return $value;
   }
 
 
@@ -231,36 +110,7 @@ public class DoubleStandardErrorBeed
         standardError = null;
       }
     }
-    $value = standardError;
-  }
-
-
-  private Double $value = null;
-
-
-  /**
-   * @post  result != null;
-   * @post  result.getSource() == this;
-   * @post  result.getOldDouble() == null;
-   * @post  result.getNewDouble() == getDouble();
-   * @post  result.getEdit() == null;
-   * @post  result.getEditState() == null;
-   */
-  @Override
-  protected final DoubleEvent createInitialEvent() {
-    return new ActualDoubleEvent(this, null, getDouble(), null);
-  }
-
-  @Override
-  protected String otherToStringInformation() {
-    return getDouble() + " (# " + getSource().get().size() + ")";
-  }
-
-  @Override
-  public void toString(StringBuffer sb, int level) {
-    super.toString(sb, level);
-    sb.append(indent(level + 1) + "value:" + getDouble() + "\n");
-    sb.append(indent(level + 1) + "number of elements in source:" + getSource().get().size() + "\n");
+    setValue(standardError);
   }
 
 }

38ff13db57235a6831feb5530827f73b8bcb5801
jand <jand@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-16 14:27:06 +0000

    Renamed "Double" beeds to "Fp" beeds in package real.fp

---
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/number/real/fp/AbstractFpConstantUnaryExpressionBeed.java
@@ -0,0 +1,57 @@
+/*<license>
+Copyright 2007 - $Date$ by the authors mentioned below.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.beedra_II.property.number.real.fp;
+
+
+import org.beedra_II.aggregate.AggregateBeed;
+import org.beedra_II.property.number.AbstractUnaryExpressionBeed;
+import org.ppeew.annotations_I.vcs.CvsInfo;
+
+
+/**
+ * <p>General code for Double implementations of {@link AbstractUnaryExpressionBeed}
+ *   that use a constant..</p>
+ */
+@CvsInfo(revision = "$Revision$",
+         date     = "$Date$",
+         state    = "$State$",
+         tag      = "$Name$")
+public abstract class AbstractFpConstantUnaryExpressionBeed
+    extends AbstractFpUnaryExpressionBeed {
+
+  /**
+   * @pre   owner != null;
+   * @post  getInteger() == null;
+   * @post  getArgument() == null;
+   * @post  getConstant() == constant;
+   */
+  public AbstractFpConstantUnaryExpressionBeed(AggregateBeed owner, Double constant) {
+    super(owner);
+    $constant = constant;
+  }
+
+  /**
+   * @basic
+   */
+  public final double getConstant() {
+    return $constant;
+  }
+
+  private final double $constant;
+
+}
+

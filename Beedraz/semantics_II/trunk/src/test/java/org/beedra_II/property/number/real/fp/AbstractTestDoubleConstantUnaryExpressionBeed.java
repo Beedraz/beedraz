45024b498866e7ef6924d3d58dfd5f2eb96c6359
jand <jand@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-16 13:21:20 +0000

    renamed decimal (double) package to real.fp (floating point)

---
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/number/real/fp/AbstractTestDoubleConstantUnaryExpressionBeed.java
@@ -0,0 +1,50 @@
+/*<license>
+ Copyright 2007 - $Date$ by the authors mentioned below.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ </license>*/
+
+package org.beedra_II.property.number.real.fp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.beedra_II.property.number.real.fp.AbstractDoubleConstantUnaryExpressionBeed;
+
+
+
+
+
+public abstract class AbstractTestDoubleConstantUnaryExpressionBeed<_DoubleBeed_ extends AbstractDoubleConstantUnaryExpressionBeed>
+    extends AbstractTestDoubleUnaryExpressionBeed<_DoubleBeed_> {
+
+  protected AbstractTestDoubleConstantUnaryExpressionBeed(double constant) {
+    $constant = constant;
+  }
+
+  protected double $constant;
+
+  @Override
+  protected final Double valueFromSubject(_DoubleBeed_ argumentBeed) {
+    return argumentBeed.getDouble();
+  }
+
+  protected final void validateConstructor(_DoubleBeed_ inb) {
+    assertEquals($aggregateBeed, inb.getOwner());
+    assertNull(inb.getArgument());
+    assertNull(inb.getDouble());
+    assertNull(inb.getDouble());
+    assertEquals($constant, inb.getConstant());
+  }
+
+}
---
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/number/real/fp/TestDoubleEBeed.java
@@ -0,0 +1,56 @@
+/*<license>
+ Copyright 2007 - $Date$ by the authors mentioned below.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ </license>*/
+
+package org.beedra_II.property.number.real.fp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.beedra_II.aggregate.AggregateBeed;
+import org.beedra_II.property.number.real.fp.DoubleEBeed;
+import org.junit.Test;
+
+
+
+
+public class TestDoubleEBeed
+    extends AbstractTestDoubleUnaryExpressionBeed<DoubleEBeed> {
+
+  @Test
+  public void testConstructor() {
+    DoubleEBeed inb = new DoubleEBeed($aggregateBeed);
+    assertEquals($aggregateBeed, inb.getOwner());
+    assertNull(inb.getArgument());
+    assertNull(inb.getDouble());
+    assertNull(inb.getDouble());
+  }
+
+  @Override
+  protected Double expectedValue(Double argumentValue) {
+    return Math.pow(Math.E, argumentValue);
+  }
+
+  @Override
+  protected DoubleEBeed createSubject(AggregateBeed owner) {
+    return new DoubleEBeed(owner);
+  }
+
+  @Override
+  protected Double valueFromSubject(DoubleEBeed argumentBeed) {
+    return argumentBeed.getDouble();
+  }
+
+}

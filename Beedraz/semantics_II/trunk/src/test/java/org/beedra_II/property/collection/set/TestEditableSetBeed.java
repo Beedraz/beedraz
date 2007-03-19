6ae11178978fe7f30d9090b1895a026cbf8c9b7a
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-19 15:57:53 +0000

    Added tests for getSize and getCardinality.

--- Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/collection/set/TestEditableSetBeed.java
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/collection/set/TestEditableSetBeed.java
@@ -19,6 +19,7 @@ package org.beedra_II.property.collection.set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,8 +27,12 @@ import java.util.Set;
 import org.beedra_II.aggregate.AggregateBeed;
 import org.beedra_II.aggregate.PropagatedEvent;
 import org.beedra_II.bean.AbstractBeanBeed;
-import org.beedra_II.event.Listener;
+import org.beedra_II.edit.EditStateException;
+import org.beedra_II.edit.IllegalEditException;
 import org.beedra_II.event.StubListener;
+import org.beedra_II.event.StubListenerMultiple;
+import org.beedra_II.property.number.integer.IntegerBeed;
+import org.beedra_II.property.number.integer.long64.ActualLongEvent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,20 +41,6 @@ public class TestEditableSetBeed {
 
   public class MyBeanBeed extends AbstractBeanBeed {
     // NOP
-  }
-
-  public class StubEditableSetBeedListener implements Listener<SetEvent<Integer>> {
-
-    public void beedChanged(SetEvent<Integer> event) {
-      $event = event;
-    }
-
-    public void reset() {
-      $event = null;
-    }
-
-    public SetEvent<Integer> $event;
-
   }
 
   @Before
@@ -61,10 +52,11 @@ public class TestEditableSetBeed {
     $setEdit = new SetEdit<Integer>($editableSetBeed);
     $setEdit.perform();
     $event1 = new ActualSetEvent<Integer>($editableSetBeed, $addedElements, $removedElements, $setEdit);
-    $listener1 = new StubListener<PropagatedEvent>();
-    $listener2 = new StubListener<PropagatedEvent>();
-    $listener3 = new StubEditableSetBeedListener();
-    $listener4 = new StubEditableSetBeedListener();
+    $listener1 = new StubListenerMultiple<PropagatedEvent>();
+    $listener2 = new StubListenerMultiple<PropagatedEvent>();
+    $listener3 = new StubListener<SetEvent<Integer>>();
+    $listener4 = new StubListener<SetEvent<Integer>>();
+    $listener5 = new StubListener<ActualLongEvent>();
   }
 
   @After
@@ -78,10 +70,11 @@ public class TestEditableSetBeed {
   private Set<Integer> $removedElements;
   private SetEdit<Integer> $setEdit;
   private SetEvent<Integer> $event1;
-  private StubListener<PropagatedEvent> $listener1;
-  private StubListener<PropagatedEvent> $listener2;
-  private StubEditableSetBeedListener $listener3;
-  private StubEditableSetBeedListener $listener4;
+  private StubListenerMultiple<PropagatedEvent> $listener1;
+  private StubListenerMultiple<PropagatedEvent> $listener2;
+  private StubListener<SetEvent<Integer>> $listener3;
+  private StubListener<SetEvent<Integer>> $listener4;
+  private StubListener<ActualLongEvent> $listener5;
 
   @Test
   public void constructor() {
@@ -90,15 +83,35 @@ public class TestEditableSetBeed {
     // add listeners to the property beed
     $owner.addListener($listener1);
     $owner.addListener($listener2);
-    assertNull($listener1.$event);
-    assertNull($listener2.$event);
+    assertNull($listener1.$event1);
+    assertNull($listener1.$event2);
+    assertNull($listener2.$event1);
+    assertNull($listener2.$event2);
     // fire a change on the registered beed
     $editableSetBeed.fireEvent($event1);
     // listeners of the aggregate beed should be notified
-    assertNotNull($listener1.$event);
-    assertNotNull($listener2.$event);
-    assertEquals($event1, $listener1.$event.getCause());
-    assertEquals($event1, $listener1.$event.getCause());
+    assertNotNull($listener1.$event1);
+    assertNotNull($listener2.$event1);
+    assertNotNull($listener1.$event2);
+    assertNotNull($listener2.$event2);
+    assertEquals($event1, $listener1.$event1.getCause());
+    assertEquals($event1, $listener2.$event1.getCause());
+    assertTrue($listener1.$event2.getCause() instanceof ActualLongEvent);
+    if ($listener1.$event2.getCause() instanceof ActualLongEvent) {
+      ActualLongEvent longEvent = (ActualLongEvent) $listener1.$event2.getCause();
+      assertEquals(longEvent.getSource(), $editableSetBeed.$sizeBeed);
+      assertEquals(longEvent.getNewLong(), new Long(0));
+      assertEquals(longEvent.getOldLong(), new Long(0));
+      assertEquals(longEvent.getEdit(), $event1.getEdit());
+    }
+    assertTrue($listener2.$event2.getCause() instanceof ActualLongEvent);
+    if ($listener2.$event2.getCause() instanceof ActualLongEvent) {
+      ActualLongEvent longEvent = (ActualLongEvent) $listener2.$event2.getCause();
+      assertEquals(longEvent.getSource(), $editableSetBeed.$sizeBeed);
+      assertEquals(longEvent.getNewLong(), new Long(0));
+      assertEquals(longEvent.getOldLong(), new Long(0));
+      assertEquals(longEvent.getEdit(), $event1.getEdit());
+    }
   }
 
   @Test
@@ -106,9 +119,14 @@ public class TestEditableSetBeed {
     Set<Integer> added1 = new HashSet<Integer>();
     added1.add(new Integer(1));
     added1.add(new Integer(2));
+    // check size
+    assertEquals($editableSetBeed.getSize().getLong(), 0L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 0L);
     // add first elements
     $editableSetBeed.addElements(added1);
     assertEquals($editableSetBeed.get(), added1);
+    assertEquals($editableSetBeed.getSize().getLong(), 2L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 2L);
     // add extra elements
     Set<Integer> added2 = new HashSet<Integer>();
     added2.add(new Integer(3));
@@ -118,6 +136,8 @@ public class TestEditableSetBeed {
     union.addAll(added1);
     union.addAll(added2);
     assertEquals($editableSetBeed.get(), union);
+    assertEquals($editableSetBeed.getSize().getLong(), 4L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 4L);
   }
 
   @Test
@@ -125,8 +145,14 @@ public class TestEditableSetBeed {
     Set<Integer> added = new HashSet<Integer>();
     added.add(new Integer(1));
     added.add(new Integer(2));
+    // check size
+    assertEquals($editableSetBeed.getSize().getLong(), 0L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 0L);
     // add elements
     $editableSetBeed.addElements(added);
+    // check size
+    assertEquals($editableSetBeed.getSize().getLong(), 2L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 2L);
     // remove elements
     Set<Integer> removed1 = new HashSet<Integer>();
     removed1.add(new Integer(2));
@@ -134,21 +160,26 @@ public class TestEditableSetBeed {
     Set<Integer> set1 = new HashSet<Integer>();
     set1.add(new Integer(1));
     assertEquals($editableSetBeed.get(), set1);
+    assertEquals($editableSetBeed.getSize().getLong(), 1L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 1L);
     // remove more elements
     Set<Integer> removed2 = new HashSet<Integer>();
     $editableSetBeed.removeElements(removed2);
     assertEquals($editableSetBeed.get(), set1);
+    assertEquals($editableSetBeed.getSize().getLong(), 1L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 1L);
     // remove more elements
     Set<Integer> removed3 = new HashSet<Integer>();
     removed3.add(new Integer(1));
     Set<Integer> setEmpty = new HashSet<Integer>();
     $editableSetBeed.removeElements(removed3);
-    added.remove(new Integer(1));
     assertEquals($editableSetBeed.get(), setEmpty);
+    assertEquals($editableSetBeed.getSize().getLong(), 0L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 0L);
   }
 
   @Test
-  public void fireEvent() {
+  public void fireEvent1() {
     // register listeners
     $editableSetBeed.addListener($listener3);
     $editableSetBeed.addListener($listener4);
@@ -157,8 +188,22 @@ public class TestEditableSetBeed {
     // checks
     assertNotNull($listener3.$event);
     assertNotNull($listener4.$event);
-    assertEquals($listener3.$event, $event1);
-    assertEquals($listener4.$event, $event1);
+    assertEquals($event1, $listener3.$event);
+    assertEquals($event1, $listener4.$event);
+  }
+
+  @Test
+  public void fireEvent2() {
+    // register listeners to the $sizeBeed
+    $editableSetBeed.$sizeBeed.addListener($listener5);
+    // fire $editableSetBeed
+    $editableSetBeed.fireEvent($event1);
+    // checks
+    assertNotNull($listener5.$event);
+    assertEquals($listener5.$event.getEdit(), $event1.getEdit());
+    assertEquals($listener5.$event.getNewLong(), 0L);
+    assertEquals($listener5.$event.getOldLong(), 0L);
+    assertEquals($listener5.$event.getSource(), $editableSetBeed.$sizeBeed);
   }
 
   @Test
@@ -169,4 +214,45 @@ public class TestEditableSetBeed {
     assertEquals(initialEvent.getRemovedElements(), new HashSet<Integer>());
     assertEquals(initialEvent.getEdit(), null);
   }
+
+  @Test
+  public void getSizeAndCardinality() throws EditStateException, IllegalEditException {
+    IntegerBeed<?> sizeBeed = $editableSetBeed.getSize();
+    assertEquals(sizeBeed.getOwner(), $editableSetBeed.getOwner());
+    assertEquals(sizeBeed.getLong(), 0L);
+    assertEquals($editableSetBeed.getSize().getLong(), 0L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 0L);
+    // add elements
+    SetEdit<Integer> setEdit = new SetEdit<Integer>($editableSetBeed);
+    setEdit.addElementToAdd(1);
+    setEdit.addElementToAdd(2);
+    setEdit.perform();
+    assertEquals(sizeBeed.getLong(), 2L);
+    assertEquals($editableSetBeed.getSize().getLong(), 2L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 2L);
+    // add elements
+    setEdit = new SetEdit<Integer>($editableSetBeed);
+    setEdit.addElementToAdd(3);
+    setEdit.perform();
+    assertEquals(sizeBeed.getLong(), 3L);
+    assertEquals($editableSetBeed.getSize().getLong(), 3L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 3L);
+    // remove elements
+    setEdit = new SetEdit<Integer>($editableSetBeed);
+    setEdit.addElementToRemove(1);
+    setEdit.addElementToRemove(3);
+    setEdit.perform();
+    assertEquals(sizeBeed.getLong(), 1L);
+    assertEquals($editableSetBeed.getSize().getLong(), 1L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 1L);
+    // remove elements
+    setEdit = new SetEdit<Integer>($editableSetBeed);
+    setEdit.addElementToRemove(2);
+    setEdit.perform();
+    assertEquals(sizeBeed.getLong(), 0L);
+    assertEquals($editableSetBeed.getSize().getLong(), 0L);
+    assertEquals($editableSetBeed.getCardinality().getLong(), 0L);
+  }
+
+
 }

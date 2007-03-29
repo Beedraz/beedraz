5e1f24324e1ec12168432303fb7d3a0970d9a78d
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-03-29 15:21:48 +0000

    Duplicate code moved to supertype.

--- Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/number/real/double64/TestDoubleGeometricMeanBeed.java
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/number/real/double64/TestDoubleGeometricMeanBeed.java
@@ -17,392 +17,57 @@
 package org.beedra_II.property.number.real.double64;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.ppeew.smallfries_I.MathUtil.equalValue;
-
 import org.beedra_II.aggregate.AggregateBeed;
-import org.beedra_II.aggregate.PropagatedEvent;
-import org.beedra_II.bean.StubBeanBeed;
-import org.beedra_II.edit.EditStateException;
-import org.beedra_II.edit.IllegalEditException;
-import org.beedra_II.event.StubListener;
-import org.beedra_II.property.collection.set.EditableSetBeed;
-import org.beedra_II.property.collection.set.SetBeed;
-import org.beedra_II.property.collection.set.SetEdit;
-import org.beedra_II.property.number.real.RealBeed;
-import org.beedra_II.property.number.real.RealEvent;
-import org.junit.After;
-import org.junit.Before;
+import org.beedra_II.event.Event;
 import org.junit.Test;
 import org.ppeew.smallfries_I.MathUtil;
 
 
-public class TestDoubleGeometricMeanBeed {
-
-  public class MyDoubleGeometricMeanBeed extends DoubleGeometricMeanBeed {
-    public MyDoubleGeometricMeanBeed(AggregateBeed owner) {
-      super(owner);
-    }
-
-    /**
-     * updateDependents is made public for testing reasons
-     */
-    public void publicUpdateDependents(ActualDoubleEvent event) {
-      updateDependents(event);
-    }
-
-    public final void publicRecalculate() {
-      assert getSource() != null;
-      recalculate(getSource());
-    }
-
-  }
-
-  @Before
-  public void setUp() throws Exception {
-    // NOP
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    // NOP
-  }
-
-  private AggregateBeed $owner = new StubBeanBeed();
-  private MyDoubleGeometricMeanBeed $doubleGeometricMeanBeed = new MyDoubleGeometricMeanBeed($owner);
-  private ActualDoubleEvent $event1 = new ActualDoubleEvent($doubleGeometricMeanBeed, new Double(0), new Double(1), null);
-      // @mudo Laatste argument mag niet null zijn??
-  private StubListener<PropagatedEvent> $listener1 = new StubListener<PropagatedEvent>();
-  private StubListener<PropagatedEvent> $listener2 = new StubListener<PropagatedEvent>();
-  private StubListener<RealEvent> $listener3 = new StubListener<RealEvent>();
+public class TestDoubleGeometricMeanBeed
+    extends AbstractTestDoubleCommonsMathSetComputationBeed<MyDoubleGeometricMeanBeed> {
 
   @Test
-  public void constructor() {
-    assertEquals($doubleGeometricMeanBeed.getOwner(), $owner);
-    assertEquals($doubleGeometricMeanBeed.getSource(), null);
-    assertEquals($doubleGeometricMeanBeed.getDouble(), null);
-    // the abstract property beed should be registered with the owner:
-    // add listeners to the property beed
-    $owner.addListener($listener1);
-    $owner.addListener($listener2);
-    assertNull($listener1.$event);
-    assertNull($listener2.$event);
-    // fire a change on the registered beed
-    $doubleGeometricMeanBeed.publicUpdateDependents($event1);
-    // listeners of the aggregate beed should be notified
-    assertNotNull($listener1.$event);
-    assertNotNull($listener2.$event);
-    assertEquals($event1, $listener1.$event.getCause());
-    assertEquals($event1, $listener1.$event.getCause());
-    // no terms registered (cannot be tested)
+  public void test() {
+    // to let the compiler know that this is a unit test
+  }
+
+  @Override
+  protected double computeStatistic(double... values) {
+    return MathUtil.geometricMean(values);
+  }
+
+  @Override
+  protected MyDoubleGeometricMeanBeed createSubject(AggregateBeed owner) {
+    return new MyDoubleGeometricMeanBeed(owner);
+  }
+
+  @Override
+  protected void recalculate(MyDoubleGeometricMeanBeed subject) {
+    subject.publicRecalculate();
+  }
+
+  @Override
+  protected void updateDependents(MyDoubleGeometricMeanBeed subject, Event event) {
+    subject.publicUpdateDependents(event);
+  }
+
+}
+
+class MyDoubleGeometricMeanBeed extends DoubleGeometricMeanBeed {
+  public MyDoubleGeometricMeanBeed(AggregateBeed owner) {
+    super(owner);
   }
 
   /**
-   * Source is null.
+   * updateDependents is made public for testing reasons
    */
-  @Test
-  public void setSource1() throws EditStateException, IllegalEditException {
-    // register listeners to the DoubleGeometricMeanBeed
-    $doubleGeometricMeanBeed.addListener($listener3);
-    assertNull($listener3.$event);
-    // check setSource
-    SetBeed<RealBeed<?>, ?> source = null;
-    $doubleGeometricMeanBeed.setSource(source);
-    assertEquals($doubleGeometricMeanBeed.getSource(), source);
-    assertEquals($doubleGeometricMeanBeed.getDouble(), null);
-    // value has not changed, so the listeners are not notified
-    assertNull($listener3.$event);
+  public void publicUpdateDependents(Event event) {
+    updateDependents(event);
   }
 
-  /**
-   * Source is effective.
-   */
-  @Test
-  public void setSource2() throws EditStateException, IllegalEditException {
-    // register listeners to the DoubleGeometricMeanBeed
-    $doubleGeometricMeanBeed.addListener($listener3);
-    assertNull($listener3.$event);
-    // check setSource
-    EditableSetBeed<RealBeed<?>> source = createSource();
-    $doubleGeometricMeanBeed.setSource(source);
-    assertEquals($doubleGeometricMeanBeed.getSource(), source);
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    // value has changed, so the listeners of the mean beed are notified
-    assertNotNull($listener3.$event);
-    assertEquals($listener3.$event.getSource(), $doubleGeometricMeanBeed);
-    assertEquals($listener3.$event.getOldDouble(), null);
-    assertEquals($listener3.$event.getNewDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    assertEquals($listener3.$event.getEdit(), null);
-    // The DoubleGeometricMeanBeed is registered as listener of the source, so when
-    // the source changes, the beed should be notified
-    $listener3.reset();
-    assertNull($listener3.$event);
-    SetEdit<RealBeed<?>> setEdit = new SetEdit<RealBeed<?>>(source);
-    EditableDoubleBeed goal = createEditableDoubleBeed(5.0);
-    setEdit.addElementToAdd(goal);
-    setEdit.perform();
-    assertNotNull($listener3.$event);
-    assertEquals($listener3.$event.getSource(), $doubleGeometricMeanBeed);
-    assertEquals($listener3.$event.getOldDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    assertEquals($listener3.$event.getNewDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0, 5.0));
-    assertEquals($listener3.$event.getEdit(), setEdit);
-    // The DoubleGeometricMeanBeed is registered as listener of all double beeds in the source,
-    // so when one of them changes, the beed should be notified
-    $listener3.reset();
-    assertNull($listener3.$event);
-    DoubleEdit doubleEdit = new DoubleEdit(goal);
-    doubleEdit.setGoal(6.0);
-    doubleEdit.perform();
-    assertNotNull($listener3.$event);
-    assertEquals($listener3.$event.getSource(), $doubleGeometricMeanBeed);
-    assertEquals($listener3.$event.getOldDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0, 5.0));
-    assertTrue(equalValue($listener3.$event.getNewDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0, 6.0), Math.ulp($listener3.$event.getNewDouble()) * 4));
-    assertEquals($listener3.$event.getEdit(), doubleEdit);
-    // When a new beed is added to the source, the DoubleGeometricMeanBeed is added as a listener
-    // of that beed. See above.
-
-    // When a beed is removed from the source, the DoubleGeometricMeanBeed is removed as listener
-    // of that beed.
-    $listener3.reset();
-    assertNull($listener3.$event);
-    setEdit = new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToRemove(goal);
-    setEdit.perform();
-    assertNotNull($listener3.$event);
-    assertEquals($listener3.$event.getSource(), $doubleGeometricMeanBeed);
-    assertTrue(equalValue($listener3.$event.getOldDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0, 6.0), Math.ulp($listener3.$event.getOldDouble()) * 4));
-    assertEquals($listener3.$event.getNewDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    assertEquals($listener3.$event.getEdit(), setEdit);
-    $listener3.reset();
-    assertNull($listener3.$event);
-    doubleEdit = new DoubleEdit(goal);
-    doubleEdit.setGoal(7.0);
-    doubleEdit.perform();
-    assertNull($listener3.$event); // the DoubleGeometricMeanBeed is NOT notified
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-
+  public final void publicRecalculate() {
+    assert getSource() != null;
+    recalculate(getSource());
   }
 
-
-  private EditableDoubleBeed createEditableDoubleBeed(Double value) {
-    try {
-      EditableDoubleBeed editableDoubleBeed = new EditableDoubleBeed($owner);
-      DoubleEdit edit = new DoubleEdit(editableDoubleBeed);
-      edit.setGoal(value);
-      edit.perform();
-      assertEquals(editableDoubleBeed.get(), value);
-      return editableDoubleBeed;
-    }
-    catch (EditStateException e) {
-      assertTrue(false);
-      return null;
-    }
-    catch (IllegalEditException e) {
-      assertTrue(false);
-      return null;
-    }
-  }
-
-
-  @Test
-  public void recalculate() throws EditStateException, IllegalEditException {
-    // create beeds
-    EditableDoubleBeed beed1 = createEditableDoubleBeed(1.0);
-    EditableDoubleBeed beed2 = createEditableDoubleBeed(2.0);
-    EditableDoubleBeed beed3 = createEditableDoubleBeed(3.0);
-    EditableDoubleBeed beed4 = createEditableDoubleBeed(4.0);
-    EditableDoubleBeed beedNull = createEditableDoubleBeed(null);
-    // double mean beed has no source
-    // cannot test: violates precondition
-//    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), null);
-    // create source
-    EditableSetBeed<RealBeed<?>> source =
-      new EditableSetBeed<RealBeed<?>>($owner);
-    // add source to mean beed
-    $doubleGeometricMeanBeed.setSource(source);
-    // recalculate (setBeed contains no elements)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), Double.NaN);
-    // add beed
-    SetEdit<RealBeed<?>> setEdit =
-      new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToAdd(beed1);
-    setEdit.perform();
-    // recalculate (setBeed contains beed 1)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0));
-    // recalculate (setBeed contains beed 1)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0));
-    // add beed
-    setEdit = new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToAdd(beed2);
-    setEdit.perform();
-    // recalculate (setBeed contains beed 1 and 2)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0));
-    // recalculate (setBeed contains beed 1 and 2)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0));
-    // add beed
-    setEdit = new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToAdd(beed3);
-    setEdit.perform();
-    // recalculate (setBeed contains beed 1, 2 and 3)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0));
-    // add beed
-    setEdit = new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToAdd(beed4);
-    setEdit.perform();
-    // recalculate (setBeed contains beed 1, 2, 3 and 4)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    // add beed
-    setEdit = new SetEdit<RealBeed<?>>(source);
-    setEdit.addElementToAdd(beedNull);
-    setEdit.perform();
-    // recalculate (setBeed contains beed 1, 2, 3, 4 and null)
-    $doubleGeometricMeanBeed.publicRecalculate();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), null);
-  }
-
-  @Test
-  public void createInitialEvent() {
-    RealEvent initialEvent = $doubleGeometricMeanBeed.createInitialEvent();
-    assertEquals(initialEvent.getSource(), $doubleGeometricMeanBeed);
-    assertEquals(initialEvent.getOldDouble(), null);
-    assertEquals(initialEvent.getNewDouble(), $doubleGeometricMeanBeed.getDouble());
-    assertEquals(initialEvent.getEdit(), null);
-  }
-
-  /**
-   * Source gooit events bij add en remove.
-   * @throws EditStateException
-   * @throws IllegalEditException
-   */
-  @Test
-  public void test() throws EditStateException, IllegalEditException {
-    // create beeds
-    EditableDoubleBeed beed1 = createEditableDoubleBeed(1.0);
-    EditableDoubleBeed beed2 = createEditableDoubleBeed(2.0);
-    EditableDoubleBeed beed3 = createEditableDoubleBeed(3.0);
-    EditableDoubleBeed beed4 = createEditableDoubleBeed(4.0);
-    // create set beed
-    EditableSetBeed<RealBeed<?>> setBeed =
-      new EditableSetBeed<RealBeed<?>>($owner);
-    // add set beed to mean beed
-    $doubleGeometricMeanBeed.setSource(setBeed);
-    // add beed
-    assertEquals($doubleGeometricMeanBeed.getDouble(), Double.NaN);
-    SetEdit<RealBeed<?>> setEdit =
-      new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed1);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed2);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed3);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed4);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed); // add an element that is already there
-    setEdit.addElementToAdd(beed4);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0, 4.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToRemove(beed1);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(2.0, 3.0, 4.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToRemove(beed2);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(3.0, 4.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToRemove(beed3);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(4.0));
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToRemove(beed4);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), Double.NaN);
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToRemove(beed4); // remove an element that is not there
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), Double.NaN);
-    // change beeds of the source
-    setBeed = new EditableSetBeed<RealBeed<?>>($owner);
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed1);
-    setEdit.perform();
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed2);
-    setEdit.perform();
-    $doubleGeometricMeanBeed.setSource(setBeed);
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0));
-    DoubleEdit doubleEdit = new DoubleEdit(beed1);
-    doubleEdit.setGoal(1.5);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.5, 2.0));
-    doubleEdit = new DoubleEdit(beed2);
-    doubleEdit.setGoal(2.5);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.5, 2.5));
-    doubleEdit = new DoubleEdit(beed1);
-    doubleEdit.setGoal(1.0);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.5));
-    doubleEdit = new DoubleEdit(beed2);
-    doubleEdit.setGoal(2.0);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0));
-    // change beeds that are added later to the source
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed3);
-    setEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.0));
-    doubleEdit = new DoubleEdit(beed3);
-    doubleEdit.setGoal(3.5);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.5));
-    doubleEdit = new DoubleEdit(beed3);
-    doubleEdit.setGoal(3.7);
-    doubleEdit.perform();
-    assertEquals($doubleGeometricMeanBeed.getDouble(), MathUtil.geometricMean(1.0, 2.0, 3.7));
-  }
-
-  private EditableSetBeed<RealBeed<?>> createSource() throws EditStateException, IllegalEditException {
-    // create set beed
-    EditableSetBeed<RealBeed<?>> setBeed =
-      new EditableSetBeed<RealBeed<?>>($owner);
-    // create beeds
-    EditableDoubleBeed beed1 = createEditableDoubleBeed(1.0);
-    EditableDoubleBeed beed2 = createEditableDoubleBeed(2.0);
-    EditableDoubleBeed beed3 = createEditableDoubleBeed(3.0);
-    EditableDoubleBeed beed4 = createEditableDoubleBeed(4.0);
-    // add beeds to set
-    SetEdit<RealBeed<?>> setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed1);
-    setEdit.perform();
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed2);
-    setEdit.perform();
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed3);
-    setEdit.perform();
-    setEdit = new SetEdit<RealBeed<?>>(setBeed);
-    setEdit.addElementToAdd(beed4);
-    setEdit.perform();
-    return setBeed;
-  }
 }

7a9608717373e9958a5dd0a7b48db18d8b986f13
cvs2svn <cvs2svn@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-02 08:03:30 +0000

    This commit was manufactured by cvs2svn to create tag 'development-37'.

---
+++ Beedraz/semantics_II/tags/development-37/src/test/java/org/beedra_II/property/number/real/double64/stat/TestDoubleSampleVarianceBeed.java
@@ -0,0 +1,74 @@
+/*<license>
+ Copyright 2007 - $Date$ by the authors mentioned below.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ </license>*/
+
+package org.beedra_II.property.number.real.double64.stat;
+
+
+import org.beedra_II.aggregate.AggregateBeed;
+import org.beedra_II.event.Event;
+import org.beedra_II.property.number.real.double64.stat.DoubleSampleVarianceBeed;
+import org.junit.Test;
+import org.ppeew.smallfries_I.MathUtil;
+
+
+public class TestDoubleSampleVarianceBeed
+    extends AbstractTestDoubleCommonsMathSetComputationBeed<MyDoubleSampleVarianceBeed>{
+
+  @Test
+  public void forceTest() {
+    // to let the compiler know that this is a unit test
+  }
+
+  @Override
+  protected double computeStatistic(double... values) {
+    return MathUtil.sampleVariance(values);
+  }
+
+  @Override
+  protected MyDoubleSampleVarianceBeed createSubject(AggregateBeed owner) {
+    return new MyDoubleSampleVarianceBeed(owner);
+  }
+
+  @Override
+  protected void recalculate(MyDoubleSampleVarianceBeed subject) {
+    subject.publicRecalculate();
+  }
+
+  @Override
+  protected void updateDependents(MyDoubleSampleVarianceBeed subject, Event event) {
+    subject.publicUpdateDependents(event);
+  }
+
+}
+
+class MyDoubleSampleVarianceBeed extends DoubleSampleVarianceBeed {
+  public MyDoubleSampleVarianceBeed(AggregateBeed owner) {
+    super(owner);
+  }
+
+  /**
+   * updateDependents is made public for testing reasons
+   */
+  public void publicUpdateDependents(Event event) {
+    updateDependents(event);
+  }
+
+  public final void publicRecalculate() {
+    assert getSource() != null;
+    recalculate(getSource());
+  }
+
+}

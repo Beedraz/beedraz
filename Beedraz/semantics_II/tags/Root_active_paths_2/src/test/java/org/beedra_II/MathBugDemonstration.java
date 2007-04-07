d41c78496205e9339fe152f995324901081622c6
cvs2svn <cvs2svn@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-07 11:26:19 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_active_paths_2'.

---
+++ Beedraz/semantics_II/tags/Root_active_paths_2/src/test/java/org/beedra_II/MathBugDemonstration.java
@@ -0,0 +1,47 @@
+package org.beedra_II;
+
+import org.apache.commons.math.stat.descriptive.moment.Variance;
+
+
+public class MathBugDemonstration {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+      // difference between getResult and evaluate
+//      double[] values = new double[] {1.0, 2.0, Double.POSITIVE_INFINITY};
+//      Variance var1 = new Variance();
+//      double value1 = var1.evaluate(values);
+//      Variance var2 = new Variance();
+//      var2.incrementAll(values);
+//      double value2 = var2.getResult();
+//      System.out.println(value1);
+//      System.out.println(value2);
+
+    // difference between getResult and evaluate
+    double[] values = new double[] {1.0, 2.0, Double.NEGATIVE_INFINITY};
+    Variance var1 = new Variance();
+    double value1 = var1.evaluate(values);
+    Variance var2 = new Variance();
+    var2.incrementAll(values);
+    double value2 = var2.getResult();
+    var2.evaluate(values);
+    System.out.println(value1);
+    System.out.println(value2);
+
+//    // evaluate method changes internal representation
+//    double[] values = new double[] {1.0, 2.0, Double.NEGATIVE_INFINITY};
+//    Variance var1 = new Variance();
+//    var1.incrementAll(values);
+//    System.out.println(var1.getN());
+//    System.out.println(var1.getResult());
+//    double value1 = var1.evaluate(values);
+//    System.out.println(var1.getN());
+//    System.out.println(var1.getResult());
+//    System.out.println(value1);
+
+  }
+
+}

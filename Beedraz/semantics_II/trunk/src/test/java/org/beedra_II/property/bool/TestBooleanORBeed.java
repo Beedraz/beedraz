682f0b4c17bfed4c7dabc734de36fb1e1bd969d1
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-16 07:50:48 +0000

    Initial commit.

---
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/bool/TestBooleanORBeed.java
@@ -0,0 +1,47 @@
+/*<license>
+ Copyright 2007 - $Date$ by the authors mentioned below.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ </license>*/
+
+package org.beedra_II.property.bool;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.beedra_II.aggregate.AggregateBeed;
+import org.junit.Test;
+
+public class TestBooleanORBeed
+    extends AbstractTestBooleanBinaryLogicalExpressionBeed<BooleanORBeed> {
+
+  @Test
+  public void testConstructor() {
+    BooleanORBeed beed = new BooleanORBeed($aggregateBeed);
+    assertEquals($aggregateBeed, beed.getOwner());
+    assertNull(beed.getLeftArgument());
+    assertNull(beed.getRightArgument());
+    assertNull(beed.get());
+  }
+
+  @Override
+  protected Boolean expectedValue(Boolean leftArgumentValue, Boolean rightArgumentValue) {
+    return leftArgumentValue || rightArgumentValue;
+  }
+
+  @Override
+  protected BooleanORBeed createSubject(AggregateBeed owner) {
+    return new BooleanORBeed(owner);
+  }
+
+}

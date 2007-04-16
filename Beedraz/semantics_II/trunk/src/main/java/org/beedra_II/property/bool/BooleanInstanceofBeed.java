71f1d6f171688bc9eab49984c412b82c860cc2f1
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-16 08:52:30 +0000

    Introduced a new superclass.

--- Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/bool/BooleanInstanceofBeed.java
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/bool/BooleanInstanceofBeed.java
@@ -17,24 +17,9 @@ limitations under the License.
 package org.beedra_II.property.bool;
 
 
-import static org.ppeew.smallfries_I.MultiLineToStringUtil.indent;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
-
-import org.beedra_II.Event;
 import org.beedra_II.aggregate.AggregateBeed;
 import org.beedra_II.bean.BeanBeed;
-import org.beedra_II.edit.Edit;
-import org.beedra_II.path.Path;
-import org.beedra_II.path.PathEvent;
-import org.beedra_II.property.AbstractPropertyBeed;
-import org.beedra_II.topologicalupdate.AbstractUpdateSourceDependentDelegate;
-import org.beedra_II.topologicalupdate.Dependent;
-import org.beedra_II.topologicalupdate.UpdateSource;
 import org.ppeew.annotations_I.vcs.CvsInfo;
-import org.ppeew.smallfries_I.MathUtil;
 
 
 /**
@@ -50,8 +35,7 @@ import org.ppeew.smallfries_I.MathUtil;
          state    = "$State$",
          tag      = "$Name$")
 public class BooleanInstanceofBeed<_BeanBeed_ extends BeanBeed>
-    extends AbstractPropertyBeed<BooleanEvent>
-    implements BooleanBeed {
+    extends AbstractBeanArgBooleanUnaryExpressionBeed<_BeanBeed_> {
 
   /**
    * @pre   owner != null;
@@ -66,119 +50,6 @@ public class BooleanInstanceofBeed<_BeanBeed_ extends BeanBeed>
     assert BeanBeed.class.isAssignableFrom(beedClass);
     $beedClass = beedClass;
   }
-
-  private final Dependent $dependent = new AbstractUpdateSourceDependentDelegate(this) {
-
-      @Override
-      protected BooleanEvent filteredUpdate(Map<UpdateSource, Event> events, Edit<?> edit) {
-        /* Events are from:
-         * - the beed,
-         * - the beed path
-         * React to the path events first, setting the corresponding beed.
-         * Then do a recalculate.
-         */
-        Boolean oldValue = get();
-        PathEvent<_BeanBeed_> beedPathEvent = (PathEvent<_BeanBeed_>)events.get($beedPath);
-        if (beedPathEvent != null) {
-          setBeed(beedPathEvent.getNewValue());
-        }
-        recalculate();
-        if (! MathUtil.equalValue(oldValue, get())) {
-          return createNewEvent(oldValue, get(), edit);
-        }
-        else {
-          return null;
-        }
-      }
-
-    };
-
-
-  public final int getMaximumRootUpdateSourceDistance() {
-    /* FIX FOR CONSTRUCTION PROBLEM
-     * At construction, the super constructor is called with the future owner
-     * of this property beed. Eventually, in the constructor code of AbstractPropertyBeed,
-     * this object is registered as update source with the dependent of the
-     * aggregate beed. During that registration process, the dependent
-     * checks to see if we need to ++ our maximum root update source distance.
-     * This involves a call to this method getMaximumRootUpdateSourceDistance.
-     * Since however, we are still doing initialization in AbstractPropertyBeed,
-     * initialization code (and construction code) further down is not yet executed.
-     * This means that our $dependent is still null, and this results in a NullPointerException.
-     * On the other hand, we cannot move the concept of $dependent up, since not all
-     * property beeds have a dependent.
-     * The fix implemented here is the following:
-     * This problem only occurs during construction. During construction, we will
-     * not have any update sources, so our maximum root update source distance is
-     * effectively 0.
-     */
-    /*
-     * TODO This only works if we only add 1 update source during construction,
-     *      so a better solution should be sought.
-     */
-    return $dependent == null ? 0 : $dependent.getMaximumRootUpdateSourceDistance();
-  }
-
-
-  /*<property name="beed">*/
-  //-----------------------------------------------------------------
-
-  /**
-   * @basic
-   */
-  public final Path<? extends _BeanBeed_> getBeedPath() {
-    return $beedPath;
-  }
-
-  /**
-   * The old beed path is removed as update source.
-   * The new beed path is added as update source.
-   * The beed is replaced by the new beed: see {@link #setBeed(BeanBeed)}.
-   */
-  public final void setBeedPath(Path<? extends _BeanBeed_> beedPath) {
-    if ($beedPath != null) {
-      $dependent.removeUpdateSource($beedPath);
-    }
-    $beedPath = beedPath;
-    if ($beedPath != null) {
-      $dependent.addUpdateSource($beedPath);
-      setBeed($beedPath.get());
-    }
-    else {
-      setBeed(null);
-    }
-  }
-
-  private Path<? extends _BeanBeed_> $beedPath;
-
-  /**
-   * @basic
-   */
-  protected final _BeanBeed_ getBeed() {
-    return $beed;
-  }
-
-  /**
-   * @post getBeed() == beed;
-   */
-  private final void setBeed(_BeanBeed_ beed) {
-    Boolean oldValue = get();
-    if ($beed != null) {
-      $dependent.removeUpdateSource($beed);
-    }
-    $beed = beed;
-    recalculate();
-    if ($beed != null) {
-      $dependent.addUpdateSource($beed);
-    }
-    if (! MathUtil.equalValue(oldValue, get())) {
-      updateDependents(createNewEvent(oldValue, get(), null));
-    }
-  }
-
-  private _BeanBeed_ $beed;
-
-  /*</property>*/
 
 
   /*<property name="beedClass">*/
@@ -196,64 +67,9 @@ public class BooleanInstanceofBeed<_BeanBeed_ extends BeanBeed>
   /*</property>*/
 
 
-  protected final BooleanEvent createNewEvent(Boolean oldValue, Boolean newValue, Edit<?> edit) {
-    return new BooleanEvent(this, oldValue, newValue, edit);
-  }
-
-
-  public boolean isEffective() {
-    return true;
-  }
-
-  public final Boolean getBoolean() {
-    return Boolean.valueOf(getboolean());
-  }
-
-  public final boolean getboolean() {
-    return $value;
-  }
-
-  public final Boolean get() {
-    return getBoolean();
-  }
-
-  private boolean $value;
-
-
-
-  private void recalculate() {
-    $value = getBeedClass().isInstance(getBeed());
-  }
-
-
-
-  public final Set<? extends UpdateSource> getUpdateSources() {
-    return $dependent.getUpdateSources();
-  }
-
-  private final static Set<? extends UpdateSource> PHI = Collections.emptySet();
-
-
-  public final Set<? extends UpdateSource> getUpdateSourcesTransitiveClosure() {
-    /* fixed to make it possible to use this method during construction,
-     * before $dependent is initialized. But that is bad code, and should be
-     * fixed.
-     */
-    return $dependent == null ? PHI : $dependent.getUpdateSourcesTransitiveClosure();
-  }
-
-
   @Override
-  public final void toString(StringBuffer sb, int level) {
-    super.toString(sb, level);
-    sb.append(indent(level + 1) + "value:" + get() + "\n");
-    sb.append(indent(level + 1) + "beed:\n");
-    if (getBeed() == null) {
-      sb.append(indent(level + 2) + "null");
-    }
-    else {
-      getBeed().toString(sb, level + 2);
-    }
+  protected void recalculate() {
+    setValue(getBeedClass().isInstance(getBeed()));
   }
 
 }
--- Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/bool/TestBooleanInstanceofBeed.java
+++ Beedraz/semantics_II/trunk/src/test/java/org/beedra_II/property/bool/TestBooleanInstanceofBeed.java
@@ -17,32 +17,12 @@
 package org.beedra_II.property.bool;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
-import org.beedra_II.StubListener;
-import org.beedra_II.aggregate.AggregateBeed;
-import org.beedra_II.aggregate.StubAggregateBeed;
 import org.beedra_II.bean.RunBeanBeed;
-import org.beedra_II.bean.WellBeanBeed;
-import org.beedra_II.edit.EditStateException;
-import org.beedra_II.edit.IllegalEditException;
-import org.beedra_II.path.ConstantPath;
-import org.beedra_II.path.NullPath;
-import org.beedra_II.path.Path;
-import org.beedra_II.path.Paths;
-import org.beedra_II.path.ToOneBeanPath;
-import org.beedra_II.property.association.set.BidirToOneEdit;
-import org.beedra_II.property.association.set.EditableBidirToOneBeed;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.ppeew.smallfries_I.MathUtil;
 
 
-public class TestBooleanInstanceofBeed {
+public class TestBooleanInstanceofBeed
+       extends AbstractTestBeanArgBooleanUnaryExpressionBeed<
+                   BooleanInstanceofBeed<?>> {
 
   public class MyRunBeanBeed extends RunBeanBeed {
     // NOP
@@ -52,172 +32,29 @@ public class TestBooleanInstanceofBeed {
     // NOP
   }
 
-  @Before
-  public void setUp() throws Exception {
-    $aggregateBeed = new StubAggregateBeed();
-    $beed = new MyRunBeanBeed();
-    $beedPath = new ConstantPath<RunBeanBeed>($beed);
-    $subject = new BooleanInstanceofBeed<RunBeanBeed>($aggregateBeed, MyRunBeanBeed.class);
-    $listener = new StubListener<BooleanEvent>();
-    // initialise runs
-    $myRun = new MyRunBeanBeed();
-    $yourRun = new YourRunBeanBeed();
-    $well1 = new WellBeanBeed();
-    $well2 = new WellBeanBeed();
-    BidirToOneEdit<RunBeanBeed, WellBeanBeed> edit =
-      new BidirToOneEdit<RunBeanBeed, WellBeanBeed>($well1.run);
-    edit.setGoal($myRun.wells);
-    edit.perform();
-    edit = new BidirToOneEdit<RunBeanBeed, WellBeanBeed>($well2.run);
-    edit.setGoal($yourRun.wells);
-    edit.perform();
-    // initialise paths
-    $pathToMyRun = new ToOneBeanPath<RunBeanBeed>(Paths.fix($well1.run));
-    $pathToYourRun = new ToOneBeanPath<RunBeanBeed>(Paths.fix($well2.run));
+  @Override
+  protected RunBeanBeed initBeed() {
+    return new MyRunBeanBeed();
   }
 
-  @After
-  public void tearDown() throws Exception {
-    $beed = null;
-    $beedPath = null;
-    $subject = null;
-    $aggregateBeed = null;
-    $listener = null;
-    $myRun = null;
-    $yourRun = null;
-    $well1 = null;
-    $well2 = null;
-    $pathToMyRun = null;
-    $pathToYourRun = null;
+  @Override
+  protected RunBeanBeed initRun1() {
+    return new MyRunBeanBeed();
   }
 
-  protected BooleanInstanceofBeed<RunBeanBeed> $subject;
-  protected AggregateBeed $aggregateBeed;
-  private RunBeanBeed $beed;
-  private Path<RunBeanBeed> $beedPath;
-  StubListener<BooleanEvent> $listener;
-  private RunBeanBeed $myRun;
-  private RunBeanBeed $yourRun;
-  private WellBeanBeed $well1;
-  private WellBeanBeed $well2;
-  private ToOneBeanPath<RunBeanBeed> $pathToMyRun;
-  private ToOneBeanPath<RunBeanBeed> $pathToYourRun;
-
-  @Test
-  public void testSetArgument_1() {
-    $subject.addListener($listener);
-    $subject.setBeedPath(new NullPath<RunBeanBeed>());
-    validateSubjectFromBeed(null);
-    validateEvent(null, null);
+  @Override
+  protected RunBeanBeed initRun2() {
+    return new YourRunBeanBeed();
   }
 
-  @Test
-  public void testSetArgument_2() {
-    $subject.addListener($listener);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($beed);
-    validateEvent(null, $beed);
+  @Override
+  protected BooleanInstanceofBeed<?> initSubject() {
+    return new BooleanInstanceofBeed<RunBeanBeed>($aggregateBeed, MyRunBeanBeed.class);
   }
 
-  private boolean valueFrom(RunBeanBeed beed) {
+  @Override
+  protected boolean valueFrom(RunBeanBeed beed) {
     return beed instanceof MyRunBeanBeed;
-  }
-
-  private void validateSubjectFromBeed(RunBeanBeed beed) {
-    assertEquals(beed, $subject.getBeed());
-    assertNotNull($subject.get());
-    assertEquals(valueFrom(beed), $subject.getboolean());
-  }
-
-  @Test
-  public void testSetArgument_3() {
-    $subject.addListener($listener);
-    changeBeedPath($pathToMyRun);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($myRun);
-    validateEvent(null, $myRun);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($myRun);
-    validateEvent($myRun, $myRun);
-    $subject.setBeedPath(null);
-    validateSubjectFromBeed(null);
-    validateEvent($myRun, null);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($myRun);
-    validateEvent(null, $myRun);
-    changeBeedPath($pathToYourRun);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($yourRun);
-    validateEvent($myRun, $yourRun);
-    changeBeedPath($pathToYourRun);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($yourRun);
-    validateEvent($yourRun, $yourRun);
-  }
-
-  @Test
-  public void testDynamics() throws EditStateException, IllegalEditException {
-    $subject.addListener($listener);
-    assertTrue($subject.isListener($listener));
-    changeBeedPath($pathToMyRun);
-    $subject.setBeedPath($beedPath);
-    validateSubjectFromBeed($myRun);
-    validateEvent(null, $myRun);
-    // now change the value of the beed path
-    changeBeedPath($pathToMyRun, $yourRun);
-    validateSubjectFromBeed($yourRun);
-    validateEvent($myRun, $yourRun);
-    changeBeedPath($pathToMyRun, $myRun);
-    validateSubjectFromBeed($myRun);
-    validateEvent($yourRun, $myRun);
-    changeBeedPath($pathToMyRun, $myRun);
-    validateSubjectFromBeed($myRun);
-    validateEvent($myRun, $myRun);
-    changeBeedPath($pathToMyRun, null);
-    validateSubjectFromBeed(null);
-    validateEvent($myRun, null);
-    changeBeedPath($pathToMyRun, null);
-    validateSubjectFromBeed(null);
-    validateEvent(null, null);
-  }
-
-  private void validateEvent(RunBeanBeed oldBeed, RunBeanBeed newBeed) {
-    Boolean expectedOldValue = valueFrom(oldBeed);
-    Boolean expectedNewValue = valueFrom(newBeed);
-    if (! MathUtil.equalValue(expectedOldValue, expectedNewValue)) {
-      assertNotNull($listener.$event);
-      assertNotNull($listener.$event.getOldValue());
-      assertTrue(MathUtil.equalValue(expectedOldValue, $listener.$event.getOldValue()));
-      assertNotNull($listener.$event.getNewValue());
-      assertTrue(MathUtil.equalValue(expectedNewValue, $listener.$event.getNewValue()));
-    }
-    else {
-      assertNull($listener.$event);
-    }
-    $listener.$event = null;
-  }
-
-  private void changeBeedPath(Path<RunBeanBeed> path) {
-    $beedPath = path;
-    $beed = path.get();
-  }
-
-  /**
-   * The given path is a path from a well to a run.
-   * Change the run of that well to the given run
-   *
-   * @pre  path != null;
-   * @pre  path.getToOneBeed() instanceof EditableBidirToOneBeed<RunBeanBeed, WellBeanBeed>;
-   * @pre  path.getToOneBeed().getOwner() != null;
-   */
-  private void changeBeedPath(ToOneBeanPath<RunBeanBeed> path, RunBeanBeed run) throws EditStateException, IllegalEditException {
-    @SuppressWarnings("unchecked")
-    EditableBidirToOneBeed<RunBeanBeed, WellBeanBeed> bidirToOne =
-      (EditableBidirToOneBeed<RunBeanBeed, WellBeanBeed>) path.getToOneBeed();
-    WellBeanBeed well = bidirToOne.getOwner();
-    BidirToOneEdit<RunBeanBeed, WellBeanBeed> edit = new BidirToOneEdit<RunBeanBeed, WellBeanBeed>(well.run);
-    edit.setGoal(run != null ? run.wells : null);
-    edit.perform();
   }
 
 }

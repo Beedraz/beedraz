441ac31b8aacf5dc83d07ce37ea1924381defe08
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-18 16:12:26 +0000

    Added extra Boolean beeds.

--- Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/bool/BooleanBeeds.java
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/bool/BooleanBeeds.java
@@ -39,12 +39,30 @@ import org.ppeew.annotations_I.vcs.CvsInfo;
 public class BooleanBeeds {
 
 
+  /*<section name="true - false">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed trueB() {
+    return new BooleanTrueBeed();
+  }
+
+  public static BooleanBeed falseB() {
+    return new BooleanFalseBeed();
+  }
+
+  /*</section>*/
+
+
   /*<section name="not">*/
   //------------------------------------------------------------------
 
   public static BooleanBeed not(BooleanBeed operand) {
+    return not(fix(operand));
+  }
+
+  public static BooleanBeed not(Path<? extends BooleanBeed> operandPath) {
     BooleanNotBeed notBeed = new BooleanNotBeed();
-    notBeed.setOperandPath(fix(operand));
+    notBeed.setOperandPath(operandPath);
     return notBeed;
   }
 
@@ -54,6 +72,9 @@ public class BooleanBeeds {
   /*<section name="and">*/
   //------------------------------------------------------------------
 
+  /**
+   * @pre operands != null;
+   */
   public static BooleanBeed and(BooleanBeed... operands) {
     switch (operands.length) {
       case 0:
@@ -121,12 +142,67 @@ public class BooleanBeeds {
   /*</section>*/
 
 
+  /*<section name="xor">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed xor(BooleanBeed... operands) {
+    switch (operands.length) {
+      case 0:
+        return new BooleanFalseBeed();
+      default:
+        return xor(operands, 0);
+    }
+  }
+
+  /**
+   * @pre operands != null;
+   * @pre operands.length >= 1;
+   * @pre start >= 0;
+   * @pre start < operands.length;
+   */
+  private static BooleanBeed xor(BooleanBeed[] operands, int start) {
+    if (start < operands.length - 1) {
+      // recursion: xor(operands, start) = operands[start] XOR xor(operands, start + 1);
+      BooleanXORBeed xorBeed = new BooleanXORBeed();
+      xorBeed.setLeftOperandPath(fix(operands[start]));
+      xorBeed.setRightOperandPath(fix(xor(operands, start + 1)));
+      return xorBeed;
+    }
+    else {
+      assert start == operands.length - 1;
+      return operands[start];
+    }
+  }
+
+  /*</section>*/
+
+
+  /*<section name="null">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed nullV(RealBeed<?> operand) {
+    return nullV(fix(operand));
+  }
+
+  public static BooleanBeed nullV(Path<? extends RealBeed<?>> operandPath) {
+    BooleanNullBeed nullBeed = new BooleanNullBeed();
+    nullBeed.setOperandPath(operandPath);
+    return nullBeed;
+  }
+
+  /*</section>*/
+
+
   /*<section name="notNull">*/
   //------------------------------------------------------------------
 
-  public static BooleanBeed notNull(RealBeed<?> operand) {
+  public static BooleanBeed notNullV(RealBeed<?> operand) {
+    return notNullV(fix(operand));
+  }
+
+  public static BooleanBeed notNullV(Path<? extends RealBeed<?>> operandPath) {
     BooleanNotNullBeed notNullBeed = new BooleanNotNullBeed();
-    notNullBeed.setOperandPath(fix(operand));
+    notNullBeed.setOperandPath(operandPath);
     return notNullBeed;
   }
 
@@ -185,16 +261,130 @@ public class BooleanBeeds {
   /*<section name="notNull">*/
   //------------------------------------------------------------------
 
-  public static <_BeanBeed_ extends BeanBeed> BooleanBeed notNull(_BeanBeed_ beanBeed) {
-    return notNull(fix(beanBeed));
+  public static <_BeanBeed_ extends BeanBeed> BooleanBeed notNullB(_BeanBeed_ beanBeed) {
+    return notNullB(fix(beanBeed));
   }
 
-  public static <_BeanBeed_ extends BeanBeed> BooleanBeed notNull(
+  public static <_BeanBeed_ extends BeanBeed> BooleanBeed notNullB(
       Path<? extends _BeanBeed_> beanBeedPath) {
     BooleanNotNullBeanBeedBeed<_BeanBeed_> notNullBeed =
       new BooleanNotNullBeanBeedBeed<_BeanBeed_>();
     notNullBeed.setBeedPath(beanBeedPath);
     return notNullBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="eq">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed eq(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return eq(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed eq(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanEQBeed eqBeed = new BooleanEQBeed();
+    eqBeed.setLeftOperandPath(leftOperandPath);
+    eqBeed.setRightOperandPath(rightOperandPath);
+    return eqBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="ne">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed ne(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return ne(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed ne(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanNEBeed neBeed = new BooleanNEBeed();
+    neBeed.setLeftOperandPath(leftOperandPath);
+    neBeed.setRightOperandPath(rightOperandPath);
+    return neBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="ge">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed ge(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return ge(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed ge(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanGEBeed geBeed = new BooleanGEBeed();
+    geBeed.setLeftOperandPath(leftOperandPath);
+    geBeed.setRightOperandPath(rightOperandPath);
+    return geBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="gt">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed gt(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return gt(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed gt(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanGTBeed gtBeed = new BooleanGTBeed();
+    gtBeed.setLeftOperandPath(leftOperandPath);
+    gtBeed.setRightOperandPath(rightOperandPath);
+    return gtBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="le">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed le(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return le(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed le(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanLEBeed leBeed = new BooleanLEBeed();
+    leBeed.setLeftOperandPath(leftOperandPath);
+    leBeed.setRightOperandPath(rightOperandPath);
+    return leBeed;
+  }
+
+  /*</section>*/
+
+
+  /*<section name="lt">*/
+  //------------------------------------------------------------------
+
+  public static BooleanBeed lt(RealBeed<?> leftOperand, RealBeed<?> rightOperand) {
+    return lt(fix(leftOperand), fix(rightOperand));
+  }
+
+  public static BooleanBeed lt(
+      Path<? extends RealBeed<?>> leftOperandPath,
+      Path<? extends RealBeed<?>> rightOperandPath) {
+    BooleanLTBeed ltBeed = new BooleanLTBeed();
+    ltBeed.setLeftOperandPath(leftOperandPath);
+    ltBeed.setRightOperandPath(rightOperandPath);
+    return ltBeed;
   }
 
   /*</section>*/

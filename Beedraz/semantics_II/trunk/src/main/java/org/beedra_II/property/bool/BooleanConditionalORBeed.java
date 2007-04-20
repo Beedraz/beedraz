5bc54ddfdece4da8ec0ed210438f6ec64d582fce
nsmeets <nsmeets@271e22cf-c92e-0410-8048-c982e1b13372>
2007-04-20 08:37:17 +0000

    Initial commit.

---
+++ Beedraz/semantics_II/trunk/src/main/java/org/beedra_II/property/bool/BooleanConditionalORBeed.java
@@ -0,0 +1,74 @@
+/*<license>
+Copyright 2007 - $Date$ by the authors mentioned below.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.beedra_II.property.bool;
+
+
+import org.ppeew.annotations_I.vcs.CvsInfo;
+
+
+/**
+ * A binary expression representing the conditional OR.
+ *
+ *  left operand  |  left value  | right operand |  right value  |  OR
+ * ----------------------------------------------------------------------
+ *       null     |      x       |       x       |       x       |  null
+ *     not null   |    null      |       x       |       x       |  null
+ *     not null   |      T       |       x       |       x       |   T
+ *     not null   |      F       |      null     |       x       |  null
+ *     not null   |      F       |   not null    |     null      |  null
+ *     not null   |      F       |   not null    |       F       |   F
+ *     not null   |      F       |   not null    |       T       |   T
+ *
+ * @invar getLeftArg() == null || getLeftArg().get() == null
+ *          ==> get() == null;
+ * @invar getLeftArg() != null && getLeftArg().get() == true
+ *          ==> get() == true;
+ * @invar getLeftArg() != null && getLeftArg().get() == false && getRightArg() == null
+ *          ==> get() == null;
+ * @invar getLeftArg() != null && getLeftArg().get() == false && getRightArg().get() == null
+ *          ==> get() == null;
+ * @invar getLeftArg() != null && getLeftArg().get() == false && getRightArg().get() == false
+ *          ==> get() == false;
+ * @invar getLeftArg() != null && getLeftArg().get() == false && getRightArg().get() == true
+ *          ==> get() == true;
+ */
+@CvsInfo(revision = "$Revision$",
+         date     = "$Date$",
+         state    = "$State$",
+         tag      = "$Name$")
+public class BooleanConditionalORBeed extends AbstractBooleanConditionalBinaryExpressionBeed {
+
+
+  @Override
+  protected boolean isDecisive(BooleanBeed leftOperand) {
+    return leftOperand.getboolean();
+  }
+
+
+  @Override
+  protected void recalculateConditionalFrom(BooleanBeed leftOperand, BooleanBeed rightOperand) {
+    setValue(leftOperand.getboolean() || rightOperand.getboolean());
+  }
+
+
+  @Override
+  public final String getOperatorString() {
+    return "||";
+  }
+
+}
+

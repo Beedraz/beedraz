70b430e8b1a53585c066a8f414e4d48a9698a996
jandockx <jandockx@271e22cf-c92e-0410-8048-c982e1b13372>
2007-05-10 12:22:05 +0000

    Documentation

--- trunk/semantics/src/main/java/org/beedraz/semantics_II/Event.java
+++ trunk/semantics/src/main/java/org/beedraz/semantics_II/Event.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2007 - $Date$ by the authors mentioned below.
+Copyright 2007 - $Date:2007-05-08 16:13:31 +0200 (Tue, 08 May 2007) $ by the authors mentioned below.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -26,52 +26,43 @@ import org.ppeew.annotations_I.vcs.SvnInfo;
 
 
 /**
- * <p>Notifies interested {@link Listener Listeners} of changes
- *   in {@link #getChangedBeed()}.</p>
- * <p>Events should be <em>immutable</em> during event propagation.
- *   Non-abstract subtypes should be declared {@code final} to ensure
- *   immutability. All mutating operations, if any, should throw an
- *   exception if called during event propagation.</p>
- * <p>The type parameter {@code _ChangeBeed_} says what the static
- *   type is of the beeds that send this event. Via polymorphism,
- *   the event can also be send by subtypes of {@code _ChangeBeed_}.
- *   The actual type parameter {@code _ChangeBeed_} in concrete
- *   generic invocations has to be a subtype of
- *
- *   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- *
- *   {@link Beed}{@code &lt;? super _This_&gt;},
- *   which means that the {@link Beed} type has to declare the static
- *   type of the events it sends as the generic invocation itself or
- *   one of its XXX
- *
- *   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- *
- *   class of actual sending beed > _ChangeBeed_
- *
- * <p>Events are specific for the type of {@link Beed} that sends them.
- *   The event type hierarchy is <em>covariant</em> with the {@link Beed}
- *   hierarchy. The type of the events sent by a beed of type {@code B} is
- *   {@code Event&lt;B&gt;}, which should be {@code final}.</p>
- * <p>A concrete event can eventually be send by {@link Beed Beeds} of type
- *   the {@code _Source_} and subtypes.</p>
- * <p>The edit is the final cause of this event. This can be {@code null},
- *   for example when the value of derived beeds changes due to structure
- *   changes, or for initial events.</p>
- *
- * <p>We cannot extend {@code java.util.EventObject}, because in this framework
+ * <p>Notifies interested {@link Listener Listeners} and
+ *   {@link Dependent Dependents} of changes in {@link #getSource() a beed}.
+ *   The change is described in full by properties of the event,
+ *   if possible, either as a delta, or as an old and a new state.
+ *   If the change was finally the result of {@link Edit#perform() performing},
+ *   {@link Edit#undo() undoing}, or {@link Edit#redo() redoing} an
+ *   {@link Edit}, a {@link #getEdit() reference to that edit} is carried
+ *   by the event. The same edit is carried by events that signal changes
+ *   in beeds that dependent on the editable beed that was changed by the
+ *   edit. This means that the {@link Edit#getTarget() edit target} is not necessarily
+ *   the same as the {@link #getSource() event source}. If the change was
+ *   the result of a structural change, such as adding, removing or changing an
+ *   operator, the {@link #getEdit()} is {@code null}.</p>
+ * <p>Events should be <em>immutable</em> during event propagation and after event
+ *   delivery. All mutating operations, if any, should throw an
+ *   exception if called after the start of event propagation. Code that creates
+ *   and sends the event is responsible for making sure the event is immutable.</p>
+ * <p>Events in general are not generic. Experiments with a generic
+ *   {@link #getSource()} or {@link #getEdit()} type showed that this does
+ *   not enhance the stability of the interface.</p>
+ * <p>Events are specific for the general type of {@link Beed} that sends them.
+ *   In general, the intention is to have a specific event type for each
+ *   Beed subtype interface. As a consequence, the event interface hierarchy is
+ *   <em>covariant</em> with the {@link Beed} interface hierarchy.</p>
+ * <p>Note that we cannot extend {@code java.util.EventObject}, because in this framework
  *   event types need multiple inheritance, so we must be able to define them as
- *   interfaces (and {@code java.util.EventObject} is a class)/</p>
+ *   interfaces (and {@code java.util.EventObject} is a class).</p>
  *
  * @author Jan Dockx
  *
  * @invar getSource() != null;
  * @invar (getEdit() != null) ? getEdit().getState() == DONE || getEdit().getState() == UNDONE;
  */
-@Copyright("2007 - $Date$, Beedraz authors")
+@Copyright("2007 - $Date:2007-05-08 16:13:31 +0200 (Tue, 08 May 2007) $, Beedraz authors")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision$",
-         date     = "$Date$")
+@SvnInfo(revision = "$Revision:849 $",
+         date     = "$Date:2007-05-08 16:13:31 +0200 (Tue, 08 May 2007) $")
 public interface Event {
 
   /**

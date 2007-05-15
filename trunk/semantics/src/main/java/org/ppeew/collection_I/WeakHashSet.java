17513d2e3d09e0684f7d671d1cd2723cebc808eb
jandockx <jandockx@271e22cf-c92e-0410-8048-c982e1b13372>
2007-05-15 17:01:48 +0000

    Total rewrite for memory efficiency. All test here and in qbaseplus are GREEN, but there are no real tests yet for this particular class. THERE WILL BE ERRORS HERE, I presume!

--- trunk/semantics/src/main/java/org/ppeew/collection_I/WeakHashSet.java
+++ trunk/semantics/src/main/java/org/ppeew/collection_I/WeakHashSet.java
@@ -23,8 +23,7 @@ import java.lang.ref.WeakReference;
 import java.util.AbstractSet;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.NoSuchElementException;
 
 import org.ppeew.annotations_I.Copyright;
 import org.ppeew.annotations_I.License;
@@ -32,9 +31,13 @@ import org.ppeew.annotations_I.vcs.SvnInfo;
 
 
 /**
- * A set that stores elements in {@link WeakReference weak references}.
- * This means that membership in the set is nevere a reason not to be garbage
- * collected.
+ * <p>A set that stores elements in {@link WeakReference weak references}.
+ *   This means that membership in the set is never a reason not to be garbage
+ *   collected.</p>
+ * <p>Since this set is used very much in an application we are developing, and
+ *   we are having memory issues, this implementation is tweaked to use
+ *   as little memory as possible.</p>
+ * <p>Cloning a weak hash set makes no sense. Use {@link #strongClone()} instead.</p>
  *
  * @author Jan Dockx
  * @author PeopleWare n.v.
@@ -43,74 +46,294 @@ import org.ppeew.annotations_I.vcs.SvnInfo;
 @License(APACHE_V2)
 @SvnInfo(revision = "$Revision$",
          date     = "$Date$")
-public class WeakHashSet<E> extends AbstractSet<E> implements Cloneable {
+public class WeakHashSet<E extends Object> extends AbstractSet<E> {
 
-  /**
-   * We only use keys in the backing weak hash $wrapped. This dummy
-   * object is used as value.
-   */
-  private final static Object DUMMY = new Object();
+  private final static int INITIAL_SIZE = 2;
 
-  @Override
-  public final Iterator<E> iterator() {
-    return $wrapped.keySet().iterator();
-  }
+  private final static int GROWTH = 2;
 
   @Override
   public final int size() {
-    return $wrapped.size();
+    optimize();
+    return $dataEnd == -1 ? 0 : $dataEnd;
   }
 
   @Override
   public final boolean isEmpty() {
-    return $wrapped.isEmpty();
+    optimize();
+    return $data == null;
+  }
+
+  private class LocalIterator implements Iterator<E> {
+
+    public LocalIterator() {
+      optimize();
+      if ($data == null) {
+        @SuppressWarnings("unchecked")
+        E[] newArray = (E[])new Object[0];
+        $localData = newArray;
+      }
+      else {
+        @SuppressWarnings("unchecked")
+        E[] newArray = (E[])new Object[$dataEnd == -1 ? 0 : $dataEnd];
+        int sourceI = 0;
+        int targetI = 0;
+        while (sourceI < $dataEnd) {
+          WeakReference<E> handle = $data[sourceI];
+          if (handle != null) {
+            E e = handle.get();
+            if (e != null) {
+              newArray[targetI] = e;
+              targetI++;
+            }
+          }
+          sourceI++;
+        }
+        @SuppressWarnings("unchecked")
+        E[] newArray2 = (E[])new Object[targetI];
+        System.arraycopy(newArray, 0, newArray2, 0, targetI);
+        $localData = newArray2;
+      }
+    }
+
+    private E[] $localData;
+    private int $position = 0;
+
+    public final boolean hasNext() {
+      return $position < $localData.length;
+    }
+
+    public final E next() throws NoSuchElementException {
+      if ($position >= $localData.length) {
+        throw new NoSuchElementException();
+      }
+      E result = $localData[$position];
+      $position++;
+      return result;
+    }
+
+    public final void remove() {
+      if ($position <= 0) {
+        throw new IllegalStateException();
+      }
+      WeakHashSet.this.remove($localData[$position -  1]);
+    }
+
   }
 
   @Override
-  public final boolean contains(Object o) {
-    return $wrapped.containsKey(o);
-  }
-
-  @Override
-  public final boolean add(E e) {
-    Object dummy = $wrapped.put(e, DUMMY);
-    return dummy == null; // e was not yet in the map
-  }
-
-  @Override
-  public final boolean remove(Object o) {
-    Object dummy = $wrapped.remove(o);
-    return dummy != null; // there actually was an o in the map
+  public final Iterator<E> iterator() {
+    return new LocalIterator();
   }
 
   @Override
   public final void clear() {
-    $wrapped.clear();
+    $data = null;
+    $dataEnd = -1;
+  }
+
+  private void optimize() {
+    if ($data != null) {
+      int i = 0;
+      while (i < $dataEnd) {
+        if (($data[i] == null) || ($data[i].get() == null)) {
+          // move from end to here
+          $dataEnd--;
+          $data[i] = $data[$dataEnd];
+          $data[$dataEnd] = null;
+          // don't progress, we have to check the new entry at i too (if i was not $dataEnd - 1)
+        }
+        else {
+          // else; we need to progress
+          i++;
+        }
+      }
+      // maybe we want to shrink the array
+      if ($dataEnd == 0) {
+        // we are empty; throw everything away to save space
+        $data = null;
+        $dataEnd = -1;
+      }
+      else {
+        shrink();
+      }
+    }
   }
 
   @Override
-  public final WeakHashSet<E> clone() {
-    try {
-      @SuppressWarnings("unchecked")
-      WeakHashSet<E> result = (WeakHashSet<E>)super.clone();
-      Map<E, Object> resultMap = new WeakHashMap<E, Object>($wrapped); // WeakHashMap is not Cloneable
-      result.$wrapped = resultMap;
-      return result;
+  public final boolean contains(Object o) {
+    if (o == null) {
+      return false;
     }
-    catch (CloneNotSupportedException cnsExc) {
-      assert false : "should not happen";
-      return null; // keep compiler happy
+    if ($data == null) {
+      return false;
+    }
+    int i = 0;
+    boolean found = false;
+    while ((i < $dataEnd) && (! found)) {
+      if (($data[i] == null) || ($data[i].get() == null)) {
+        // move from end to here
+        $dataEnd--;
+        $data[i] = $data[$dataEnd];
+        $data[$dataEnd] = null;
+        // don't progress, we have to check the new entry at i too (if i was not $dataEnd - 1)
+      }
+      else {
+        assert $data[i] != null;
+        assert $data[i].get() != null;
+//        assert o != null;
+        if (o.equals($data[i].get())) {
+          // o is in the set
+          found = true;
+        }
+        // else; we need to continue looking for the element until we reach the end
+        i++;
+      }
+    }
+    shrink();
+    // if we get here, o is not in the set
+    return found;
+  }
+
+  /**
+   * @throws NullPointerException
+   *         e == null;
+   */
+  @Override
+  public final boolean add(E e) throws NullPointerException {
+    if (e == null) {
+      throw new NullPointerException("cannot add null to a WeakHashSet");
+    }
+    if ($data == null) {
+      @SuppressWarnings("unchecked")
+      WeakReference<E>[] newArray = (WeakReference<E>[])new WeakReference<?>[INITIAL_SIZE];
+      $data = newArray;
+      $dataEnd = 0;
+    }
+    int i = 0;
+    while (i < $dataEnd) {
+      if (($data[i] == null) || ($data[i].get() == null)) {
+        // move from end to here
+        $dataEnd--;
+        $data[i] = $data[$dataEnd];
+        $data[$dataEnd] = null;
+        // don't progress, we have to check the new entry at i too (if i was not $dataEnd - 1)
+      }
+      else {
+        assert $data[i] != null;
+        assert $data[i].get() != null;
+//        assert e != null;
+        if (e.equals($data[i].get())) {
+          // e is already in the set; we are done, and the set did not change
+          return false;
+        }
+        // else; we need to continue looking for the element until we reach the end
+        i++;
+      }
+    }
+    shrink();
+    /* if we get here, we have not found e in the array,
+     * and i == $dataEnd, and everything < i is cleaned up;
+     * we will add the element at $dataEnd, and grow it,
+     * if there is room; if not, we will create a new, bigger array,
+     * and copy entries; in any case, we will return true
+     */
+    if ($dataEnd == $data.length) {
+      // the array is full
+      @SuppressWarnings("unchecked")
+      WeakReference<E>[] newArray = (WeakReference<E>[])new WeakReference<?>[$data.length + GROWTH];
+      System.arraycopy($data, 0, newArray, 0, $dataEnd);
+      $data = newArray;
+    }
+    // there is still room in the array
+    $data[$dataEnd] = new WeakReference<E>(e);
+    $dataEnd++;
+    return true;
+  }
+
+  @Override
+  public final boolean remove(Object o) {
+    if (o == null) {
+      return false;
+    }
+    if ($data == null) {
+      return false;
+    }
+    int i = 0;
+    boolean removed = false;
+    while (i < $dataEnd) {
+      if (($data[i] == null) || ($data[i].get() == null)) {
+        // move from end to here
+        $dataEnd--;
+        $data[i] = $data[$dataEnd];
+        $data[$dataEnd] = null;
+        // don't progress, we have to check the new entry at i too (if i was not $dataEnd - 1)
+      }
+      else {
+        if (! removed) {
+          assert $data[i] != null;
+          assert $data[i].get() != null;
+  //        assert o != null;
+          if (o.equals($data[i].get())) {
+            // found! remove! we will move the last element here
+            $dataEnd--;
+            $data[i] = $data[$dataEnd];
+            $data[$dataEnd] = null;
+            removed = true; // we will continue cleaning up
+          }
+          // else; we need to continue looking for the element until we reach the end
+        }
+        i++;
+      }
+    }
+    if ($dataEnd == 0) {
+      // we are empty; throw everything away to save space
+      $data = null;
+      $dataEnd = -1;
+    }
+    else {
+      shrink();
+    }
+    return removed;
+  }
+
+  private void shrink() {
+    if ($dataEnd + 1 + GROWTH < $data.length) {
+      // the array is to big
+      @SuppressWarnings("unchecked")
+      WeakReference<E>[] newArray = (WeakReference<E>[])new WeakReference<?>[$dataEnd + GROWTH];
+      System.arraycopy($data, 0, newArray, 0, $dataEnd);
+      $data = newArray;
     }
   }
 
   public final HashSet<E> strongClone() {
+    optimize();
     return new HashSet<E>(this);
   }
 
   /**
-   * Cannot be made final for clone.
+   * Data in this set is stored directly in an array, with only very
+   * little buffer for growth, to be as memory efficient as possible.
+   * A linked list might be a possible solution to, but that would
+   * require a small extra object for each entry. This implementation is not
+   * time efficient, but memory efficiency is our primary goal.
+   * Initially the array reference is {@code null}, to use as little
+   * space as possible. The array can contain {@code null} references,
+   * and the {@link WeakReference} entries that are effective,
+   * might refer to {@code null}.
+   * As often as possible, we will push {@code null} references
+   * to the end of the array.
    */
-  private Map<E, Object> $wrapped = new WeakHashMap<E, Object>();
+  private WeakReference<E>[] $data = null;
+
+  /**
+   * @invar $data == null ?? $dataEnd = -1;
+   * @invar $dataEnd != -1 ? for (int i : $dataEnd .. $data.lenght - 1) {
+   *                           $data[i] == null;
+   *                         }
+   */
+  private int $dataEnd = -1;
 
 }
 

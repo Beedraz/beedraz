74cf43972b7ae012db80f5f85491e0258cbf8476
nelesmeetsppw <nelesmeetsppw@271e22cf-c92e-0410-8048-c982e1b13372>
2007-06-06 14:38:20 +0000

    The get(_Key_) method of MapBeed now returns a Path<_Value_> instead of a _Value_.

--- trunk/semantics/src/main/java/org/beedraz/semantics_II/expression/collection/set/ActualMapEvent.java
+++ trunk/semantics/src/main/java/org/beedraz/semantics_II/expression/collection/set/ActualMapEvent.java
@@ -24,7 +24,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.beedraz.semantics_II.AbstractEvent;
+import org.beedraz.semantics_II.Beed;
 import org.beedraz.semantics_II.Edit;
+import org.beedraz.semantics_II.path.Path;
 import org.ppeew.annotations_I.vcs.CvsInfo;
 
 
@@ -40,7 +42,7 @@ import org.ppeew.annotations_I.vcs.CvsInfo;
          date     = "$Date$",
          state    = "$State: Exp $",
          tag      = "$Name:  $")
-public final class ActualMapEvent<_Key_, _Value_>
+public final class ActualMapEvent<_Key_, _Value_ extends Beed<?>>
     extends AbstractEvent
     implements MapEvent<_Key_, _Value_> {
 
@@ -60,41 +62,41 @@ public final class ActualMapEvent<_Key_, _Value_>
    *           : getRemovedElements().isEmpty();
    */
   public ActualMapEvent(MapBeed<_Key_, _Value_, ?> source,
-                        Map<_Key_, _Value_> addedElements,
-                        Map<_Key_, _Value_> removedElements,
+                        Map<_Key_, Path<_Value_>> addedElements,
+                        Map<_Key_, Path<_Value_>> removedElements,
                         Edit<?> edit) {
     super(source, edit);
     $addedElements = addedElements == null
-                       ? Collections.<_Key_, _Value_>emptyMap()
-                       : new HashMap<_Key_, _Value_>(addedElements);
+                       ? Collections.<_Key_, Path<_Value_>>emptyMap()
+                       : new HashMap<_Key_, Path<_Value_>>(addedElements);
     $removedElements = removedElements == null
-                         ? Collections.<_Key_, _Value_>emptyMap()
-                         : new HashMap<_Key_, _Value_>(removedElements);
+                         ? Collections.<_Key_, Path<_Value_>>emptyMap()
+                         : new HashMap<_Key_, Path<_Value_>>(removedElements);
   }
 
   /**
    * @basic
    */
-  public Map<_Key_, _Value_> getAddedElements() {
+  public Map<_Key_, Path<_Value_>> getAddedElements() {
     return Collections.unmodifiableMap($addedElements);
   }
 
  /**
    * @invar $addedElements != null;
    */
-  private final Map<_Key_, _Value_> $addedElements;
+  private final Map<_Key_, Path<_Value_>> $addedElements;
 
   /**
    * @basic
    */
-  public Map<_Key_, _Value_> getRemovedElements() {
+  public Map<_Key_, Path<_Value_>> getRemovedElements() {
     return Collections.unmodifiableMap($removedElements);
   }
 
   /**
    * @invar $removedElements != null;
    */
-  private final Map<_Key_, _Value_> $removedElements;
+  private final Map<_Key_, Path<_Value_>> $removedElements;
 
   @Override
   protected String otherToStringInformation() {

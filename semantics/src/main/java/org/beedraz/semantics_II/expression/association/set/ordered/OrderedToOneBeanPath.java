46a032c7463838bc454ed03338571d70557f858d
jandockx <jandockx@271e22cf-c92e-0410-8048-c982e1b13372>
2007-07-26 22:36:48 +0000

    Fixing Javadoc error.

--- semantics/src/main/java/org/beedraz/semantics_II/expression/association/set/ordered/OrderedToOneBeanPath.java
+++ semantics/src/main/java/org/beedraz/semantics_II/expression/association/set/ordered/OrderedToOneBeanPath.java
@@ -35,7 +35,7 @@ import org.ppeew.annotations_I.vcs.SvnInfo;
 
 /**
  * <p>{@link Path} that selects a {@link BeanBeed} from an
- *   {@link OrderedBidirToOneBeed}. The {@link #getToOneBeed() to-one beed} is the result of
+ *   {@link EditableOrderedBidirToOneBeed}. The {@link #getToOneBeed() to-one beed} is the result of
  *   a {@link #getToOnePath() property beed path}. When the result of the
  *   {@link #getToOnePath() ordered to-one beed path} changes (the path sends events),
  *   <em>or the result changes itself</em> (the {@link EditableOrderedBidirToOneBeed}

a74d1ff62adda4427e08bcb08dfe0c9ebc79fd37
nelesmeetsppw <nelesmeetsppw@271e22cf-c92e-0410-8048-c982e1b13372>
2007-08-02 13:11:53 +0000

    Initial commit.

---
+++ trunk/semantics/src/main/java/org/beedraz/semantics_II/expression/date/DateBeeds.java
@@ -0,0 +1,64 @@
+/*<license>
+Copyright 2007 - $Date: 2007-05-08 16:22:50 +0200 (di, 08 mei 2007) $ by the authors mentioned below.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.beedraz.semantics_II.expression.date;
+
+
+import static org.ppeew.annotations_I.License.Type.APACHE_V2;
+
+import java.util.Date;
+
+import org.beedraz.semantics_II.EditStateException;
+import org.beedraz.semantics_II.IllegalEditException;
+import org.beedraz.semantics_II.aggregate.AggregateBeed;
+import org.ppeew.annotations_I.Copyright;
+import org.ppeew.annotations_I.License;
+import org.ppeew.annotations_I.vcs.SvnInfo;
+
+
+/**
+ * <p>Convenience methods for working with {@link DateBeed date beeds}.</p>
+ *
+ * @author  Nele Smeets
+ * @author  Peopleware n.v.
+ */
+@Copyright("2007 - $Date: 2007-05-08 16:22:50 +0200 (di, 08 mei 2007) $, Beedraz authors")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 853 $",
+         date     = "$Date: 2007-05-08 16:22:50 +0200 (di, 08 mei 2007) $")
+public class DateBeeds {
+
+
+  /*<section name="editableDateBeed">*/
+  //------------------------------------------------------------------
+
+  public static EditableDateBeed editableDateBeed(Date constant, AggregateBeed owner) throws IllegalEditException {
+    try {
+      EditableDateBeed editableDateBeed = new EditableDateBeed(owner);
+      DateEdit edit = new DateEdit(editableDateBeed);
+      edit.setGoal(constant);
+      edit.perform();
+      return editableDateBeed;
+    }
+    catch (EditStateException e) {
+      assert false : "Shouldn't happen";
+      return null;
+    }
+  }
+
+  /*</section>*/
+}
+
